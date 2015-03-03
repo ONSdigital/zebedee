af8d4dc48401d3e035c6fd2c879acc3f041b20ae
David Carboni <david@carboni.io>
2015-03-03 14:28:39 +0000

    Test cases for move and copy.

--- src/test/java/com/github/onsdigital/zebedee/PathUtilsTest.java
+++ src/test/java/com/github/onsdigital/zebedee/PathUtilsTest.java
@@ -1,7 +1,15 @@
 package com.github.onsdigital.zebedee;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.After;
@@ -10,12 +18,16 @@ import org.junit.Test;
 
 public class PathUtilsTest {
 
+	Path folder;
+
 	@Before
 	public void setUp() throws Exception {
+		folder = Files.createTempDirectory(this.getClass().getSimpleName());
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		FileUtils.deleteDirectory(folder.toFile());
 	}
 
 	@Test
@@ -46,6 +58,115 @@ public class PathUtilsTest {
 				StringUtils.substring(result, 0, 127));
 		assertEquals(StringUtils.substring(string, -127),
 				StringUtils.substring(result, -127));
+	}
+
+	@Test
+	public void shouldCopyFile() throws IOException {
+
+		// Given
+		Path source = Files.createFile(folder.resolve("source"));
+		Path destination = folder.resolve("destination");
+
+		// When
+		PathUtils.copy(source, destination);
+
+		// Then
+		assertTrue(Files.exists(destination));
+		assertTrue(Files.exists(source));
+	}
+
+	@Test
+	public void shouldCopyFileToNonexistentFolder() throws IOException {
+
+		// Given
+		// A destination where the parent folders don't exist:
+		Path source = Files.createFile(folder.resolve("source"));
+		Path destination = folder.resolve("non/existent/folder/destination");
+
+		// When
+		PathUtils.copy(source, destination);
+
+		// Then
+		assertTrue(Files.exists(destination));
+		assertTrue(Files.exists(source));
+	}
+
+	@Test(expected = NoSuchFileException.class)
+	public void shouldNotCopyNonexistentFile() throws IOException {
+
+		// Given
+		// The source file does not actually exist:
+		Path source = folder.resolve("source");
+		Path destination = folder.resolve("destination");
+
+		// When
+		PathUtils.copy(source, destination);
+
+		// Then
+		// We should have an exception
+	}
+
+	@Test
+	public void shouldMoveFile() throws IOException {
+
+		// Given
+		Path source = Files.createFile(folder.resolve("source"));
+		Path destination = folder.resolve("destination");
+
+		// When
+		PathUtils.move(source, destination);
+
+		// Then
+		assertTrue(Files.exists(destination));
+		assertFalse(Files.exists(source));
+	}
+
+	@Test
+	public void shouldMoveFileToDestinationThatAlreadyExists()
+			throws IOException {
+
+		// Given
+		// The destination file already exists:
+		Path source = Files.createFile(folder.resolve("source"));
+		Path destination = Files.createFile(folder.resolve("destination"));
+
+		// When
+		PathUtils.move(source, destination);
+
+		// Then
+		assertTrue(Files.exists(destination));
+		assertFalse(Files.exists(source));
+	}
+
+	@Test
+	public void shouldMoveFileToNonexistentFolder() throws IOException {
+
+		// Given
+		// A destination where the parent folders don't exist:
+		Path source = Files.createFile(folder.resolve("source"));
+		Path destination = folder.resolve("non/existent/folder/destination");
+
+		// When
+		PathUtils.move(source, destination);
+
+		// Then
+		assertTrue(Files.exists(destination));
+		assertFalse(Files.exists(source));
+	}
+
+	@Test(expected = NoSuchFileException.class)
+	public void shouldNotMoveNonexistentFile() throws IOException {
+
+		// Given
+		// The source file does not actually exist:
+		Path source = folder.resolve("source");
+		Path destination = folder.resolve("destination");
+
+		// When
+		PathUtils.move(source, destination);
+
+		// Then
+		// We should have an exception
 	}
 
 }

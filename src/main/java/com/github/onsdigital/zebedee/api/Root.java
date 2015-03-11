2e7e5c1bb2874696744b9c69e4f7762af1f101ff
David Carboni <david@carboni.io>
2015-03-11 17:49:08 +0000

    Set Gson Date format and ResourceUtils.classLoaderClass.

--- src/main/java/com/github/onsdigital/zebedee/api/Root.java
+++ src/main/java/com/github/onsdigital/zebedee/api/Root.java
@@ -1,5 +1,13 @@
 package com.github.onsdigital.zebedee.api;
 
+import com.github.davidcarboni.ResourceUtils;
+import com.github.davidcarboni.restolino.framework.Startup;
+import com.github.davidcarboni.restolino.json.Serialiser;
+import com.github.onsdigital.zebedee.Content;
+import com.github.onsdigital.zebedee.Zebedee;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -10,97 +18,93 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-
-import com.github.davidcarboni.restolino.framework.Startup;
-import com.github.onsdigital.zebedee.Content;
-import com.github.onsdigital.zebedee.Zebedee;
-
 public class Root implements Startup {
 
-	static Path root;
-	static Zebedee zebedee;
+    static Path root;
+    static Zebedee zebedee;
 
-	@Override
-	public void init() {
-		try {
+    @Override
+    public void init() {
+        try {
 
-			// Create a Zebedee folder:
-			root = Files.createTempDirectory("zebedee");
-			zebedee = Zebedee.create(root);
-			Path taxonomy = Paths.get(".").resolve("target/taxonomy");
-			List<Path> content = listContent(taxonomy);
-			copyContent(content, taxonomy);
+            // Create a Zebedee folder:
+            root = Files.createTempDirectory("zebedee");
+            zebedee = Zebedee.create(root);
+            Path taxonomy = Paths.get(".").resolve("target/taxonomy");
+            List<Path> content = listContent(taxonomy);
+            copyContent(content, taxonomy);
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	private List<Path> listContent(Path taxonomy) throws IOException {
-		List<Path> content = new ArrayList<>();
+        // Set ISO date formatting in Gson:
+        Serialiser.getBuilder().setDateFormat("yyyy-MM-dd'T'HH:mmZ");
 
-		// List the taxonomy files:
-		System.out.println(taxonomy.toAbsolutePath());
-		listFiles(taxonomy, taxonomy, content);
+        // Set the class that will be used to determine a ClassLoader when loading resources:
+        ResourceUtils.classLoaderClass = Root.class;
+    }
 
-		return content;
-	}
+    private List<Path> listContent(Path taxonomy) throws IOException {
+        List<Path> content = new ArrayList<>();
 
-	private void copyContent(List<Path> content, Path taxonomy)
-			throws IOException {
+        // List the taxonomy files:
+        System.out.println(taxonomy.toAbsolutePath());
+        listFiles(taxonomy, taxonomy, content);
 
-		// Extract the content:
-		for (Path item : content) {
-			Path source = taxonomy.resolve(item);
-			Path destination = zebedee.published.path.resolve(item);
-			Files.createDirectories(destination.getParent());
-			try (InputStream input = Files.newInputStream(source);
-					OutputStream output = Files.newOutputStream(destination)) {
-				IOUtils.copy(input, output);
-			}
-		}
+        return content;
+    }
 
-		System.out.println("Zebedee root is at: " + root.toAbsolutePath());
-	}
+    private void copyContent(List<Path> content, Path taxonomy)
+            throws IOException {
 
-	/**
-	 * Recursively lists all files within this {@link Content}.
-	 * 
-	 * @param path
-	 *            The path to start from. This method calls itself recursively.
-	 * @param files
-	 *            The list to which results will be added.
-	 * @throws IOException
-	 *             If a filesystem error occurs.
-	 */
-	private static void listFiles(Path base, Path path, List<Path> files)
-			throws IOException {
-		try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
-			for (Path entry : stream) {
-				if (Files.isDirectory(entry)) {
-					listFiles(base, entry, files);
-				} else {
-					files.add(base.relativize(entry));
-				}
-			}
-		}
-	}
+        // Extract the content:
+        for (Path item : content) {
+            Path source = taxonomy.resolve(item);
+            Path destination = zebedee.published.path.resolve(item);
+            Files.createDirectories(destination.getParent());
+            try (InputStream input = Files.newInputStream(source);
+                 OutputStream output = Files.newOutputStream(destination)) {
+                IOUtils.copy(input, output);
+            }
+        }
 
-	/**
-	 * Cleans up
-	 */
-	@Override
-	protected void finalize() throws Throwable {
-		System.out.println(" - Deleting Zebeddee at: " + root);
-		try {
-			FileUtils.deleteDirectory(root.toFile());
-			System.out.println(" - Deleting Zebeddee complete: " + root);
-		} catch (Throwable t) {
-			System.out.println(" - Error deleting Zebedee: ");
-			System.out.println(t.getStackTrace());
-		}
-	}
+        System.out.println("Zebedee root is at: " + root.toAbsolutePath());
+    }
+
+    /**
+     * Recursively lists all files within this {@link Content}.
+     *
+     * @param path  The path to start from. This method calls itself recursively.
+     * @param files The list to which results will be added.
+     * @throws IOException If a filesystem error occurs.
+     */
+    private static void listFiles(Path base, Path path, List<Path> files)
+            throws IOException {
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+            for (Path entry : stream) {
+                if (Files.isDirectory(entry)) {
+                    listFiles(base, entry, files);
+                } else {
+                    files.add(base.relativize(entry));
+                }
+            }
+        }
+    }
+
+    /**
+     * Cleans up
+     */
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println(" - Deleting Zebeddee at: " + root);
+        try {
+            FileUtils.deleteDirectory(root.toFile());
+            System.out.println(" - Deleting Zebeddee complete: " + root);
+        } catch (Throwable t) {
+            System.out.println(" - Error deleting Zebedee: ");
+            System.out.println(t.getStackTrace());
+        }
+    }
 
 }

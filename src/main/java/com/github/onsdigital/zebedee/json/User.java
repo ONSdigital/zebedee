296e8e7cf459fbea62786b17c6c55ae4cd4edf77
David Carboni <david@carboni.io>
2015-03-16 15:19:43 +0000

    Javadoc tweak.

--- src/main/java/com/github/onsdigital/zebedee/json/User.java
+++ src/main/java/com/github/onsdigital/zebedee/json/User.java
@@ -1,6 +1,7 @@
 package com.github.onsdigital.zebedee.json;
 
 /**
+ * Represents a user account. NB this record intentionally does not contain any permission-related information.
  * Created by david on 12/03/2015.
  */
 public class User {

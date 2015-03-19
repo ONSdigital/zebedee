7ba7e4069d77c5da0ea55bc8e5e32be5202a662b
CarlHembrough <carlhuk@gmail.com>
2015-03-19 16:23:11 +0000

    Now serves files cross origin from tredegar and florence (localhost)

--- src/main/java/com/github/onsdigital/zebedee/api/CrossOriginFilter.java
+++ src/main/java/com/github/onsdigital/zebedee/api/CrossOriginFilter.java
@@ -4,21 +4,32 @@ import com.github.davidcarboni.restolino.framework.Filter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
 
-public class CrossOriginFilter implements Filter
-{
+public class CrossOriginFilter implements Filter {
+
+    private static final List<String> trustedDomains = Arrays.asList("http://localhost:8081", "http://localhost:8081");
+
     /**
      * This filter adds required headers to the Http response allowing cross origin calls.
      * There is no configuration to enable this filter, it is recognised automatically at run time.
+     *
      * @param request
      * @param response
      * @return
      */
     @Override
-    public boolean filter(HttpServletRequest request, HttpServletResponse response)
-    {
-        response.addHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-        response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    public boolean filter(HttpServletRequest request, HttpServletResponse response) {
+
+        String origin = request.getHeader("origin");
+
+        if (trustedDomains.contains(origin)) {
+            response.addHeader("Access-Control-Allow-Origin", origin);
+            response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            response.addHeader("Access-Control-Allow-Credentials", "true");
+        }
+
         return true;
     }
 }

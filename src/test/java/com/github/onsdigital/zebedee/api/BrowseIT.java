bdd033211374020989ca8d9e29e1e9c9bdf537d6
CarlHembrough <carlhuk@gmail.com>
2015-03-31 09:08:44 +0100

    Added authentication to API integration tests.

--- src/test/java/com/github/onsdigital/zebedee/api/BrowseIT.java
+++ src/test/java/com/github/onsdigital/zebedee/api/BrowseIT.java
@@ -1,14 +1,23 @@
 package com.github.onsdigital.zebedee.api;
 
-import com.github.onsdigital.zebedee.model.Configuration;
 import com.github.onsdigital.zebedee.json.CollectionDescription;
+import com.github.onsdigital.zebedee.model.Configuration;
 import com.jayway.restassured.response.Response;
+import org.junit.Before;
 import org.junit.Test;
 
 import static com.jayway.restassured.RestAssured.get;
 import static com.jayway.restassured.RestAssured.given;
 
 public class BrowseIT {
+
+    private String authenticationToken = "";
+
+    @Before
+    public void setUp() throws Exception {
+        authenticationToken = LoginIT.login();
+    }
+
     @Test
     public void shouldReturn404IfNoPathSpecified() {
         Response response = get(Configuration.getBaseUrl() + "/browse");
@@ -18,7 +27,7 @@ public class BrowseIT {
     @Test
     public void shouldReturn200WithValidCollectionName() {
 
-        CollectionDescription description = CollectionIT.createCollection();
+        CollectionDescription description = CollectionIT.createCollection(authenticationToken);
 
         Response response = given()
                 .get(Configuration.getBaseUrl() + "/browse/" + description.name);
--- src/test/java/com/github/onsdigital/zebedee/api/CollectionIT.java
+++ src/test/java/com/github/onsdigital/zebedee/api/CollectionIT.java
@@ -1,11 +1,12 @@
 package com.github.onsdigital.zebedee.api;
 
 
-import com.github.onsdigital.zebedee.model.Configuration;
 import com.github.onsdigital.zebedee.json.CollectionDescription;
+import com.github.onsdigital.zebedee.model.Configuration;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.jayway.restassured.response.Response;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Date;
@@ -18,30 +19,43 @@ public class CollectionIT {
 
     private static final Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX").create();
 
-    public static CollectionDescription createCollection() {
+    private String authenticationToken = "";
+
+    public static CollectionDescription createCollection(String authenticationToken) {
         CollectionDescription description = new CollectionDescription();
         description.name = UUID.randomUUID().toString();
         description.publishDate = new Date();
 
-        Response postResponse = given().body(gson.toJson(description)).post(Configuration.getBaseUrl() + "/collection");
+        Response postResponse = given()
+                .header(LoginIT.tokenHeader, authenticationToken)
+                .body(gson.toJson(description))
+                .post(Configuration.getBaseUrl() + "/collection");
         postResponse.then().assertThat().statusCode(200);
 
         return description;
     }
 
+    @Before
+    public void setUp() throws Exception {
+        authenticationToken = LoginIT.login();
+    }
+
     @Test
     public void shouldCreateCollection() {
 
-        CollectionDescription description = createCollection();
+        CollectionDescription description = createCollection(authenticationToken);
 
-        Response postResponse = given().body(gson.toJson(description)).post(Configuration.getBaseUrl() + "/collection");
+        Response postResponse = given()
+                .header("X-Florence-Token", authenticationToken)
+                .body(gson.toJson(description))
+                .post(Configuration.getBaseUrl() + "/collection");
         postResponse.then().assertThat().statusCode(409);
     }
 
     @Test
     public void shouldUpdateCollection() {
 
-        CollectionDescription description = createCollection();
+        CollectionDescription description = createCollection(authenticationToken);
 
         String oldName = description.name;
         Date oldPublishDate = description.publishDate;
@@ -60,10 +74,10 @@ public class CollectionIT {
     @Test
     public void shouldReturn409IfUpdateNameAlreadyExists() {
 
-        CollectionDescription description = createCollection();
+        CollectionDescription description = createCollection(authenticationToken);
         String existingName = description.name;
 
-        description = createCollection();
+        description = createCollection(authenticationToken);
 
         Response postResponse = given().body(gson.toJson(description)).post(Configuration.getBaseUrl() + "/collection/" + existingName);
         postResponse.then().assertThat().statusCode(409);
@@ -72,7 +86,7 @@ public class CollectionIT {
     @Test
     public void shouldGetCollection() {
 
-        CollectionDescription description = createCollection();
+        CollectionDescription description = createCollection(authenticationToken);
 
         Response response = get(Configuration.getBaseUrl() + "/collection/" + description.name);
         response.then().assertThat().statusCode(200);

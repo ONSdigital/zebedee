46f39284fdb1bc82197b9cfc598a50f5d30a06e2
Thomas Ridd <twridd@gmail.com>
2015-04-08 13:58:54 +0100

    Working on uploads

---
+++ src/main/java/com/github/onsdigital/zebedee/api/Upload.java
@@ -0,0 +1,7 @@
+package com.github.onsdigital.zebedee.api;
+
+/**
+ * Created by thomasridd on 07/04/15.
+ */
+public class Upload {
+}
---
+++ src/test/java/com/github/onsdigital/zebedee/api/UploadIT.java
@@ -0,0 +1,25 @@
+package com.github.onsdigital.zebedee.api;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UploadIT {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testUpload() throws Exception {
+
+    }
+}

7ffc58ff68e31d32a6dae6e765a078336ec1c0a3
David Carboni <david@carboni.io>
2015-04-13 14:52:29 +0100

    Adding documentation so that it can be extracted by the apidoc tool.

--- src/main/java/com/github/onsdigital/zebedee/api/Users.java
+++ src/main/java/com/github/onsdigital/zebedee/api/Users.java
@@ -1,7 +1,6 @@
 package com.github.onsdigital.zebedee.api;
 
 import com.github.davidcarboni.restolino.framework.Api;
-import com.github.onsdigital.zebedee.json.Credentials;
 import com.github.onsdigital.zebedee.json.User;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jetty.http.HttpStatus;
@@ -14,11 +13,21 @@ import javax.ws.rs.PUT;
 import java.io.IOException;
 
 /**
- * Created by david on 12/03/2015.
+ * API for managing user accounts. For password management, see {@link Password}.
  */
 @Api
 public class Users {
 
+    /**
+     * Allows you to read the details of a user account.
+     * @param request Expects an "email" get parameter.
+     * @param response <ul>
+     *                      <li>The user details with the password hash removed.</li>
+     *                      <li>If no email parameter is provided:  {@link HttpStatus#BAD_REQUEST_400}</li>
+     *                      <li>if no user exists for the giver email:  {@link HttpStatus#NOT_FOUND_404}</li>
+     *                      </ul>
+     * @return A {@link User} object.
+     */
     @GET
     public User read(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
@@ -32,15 +41,24 @@ public class Users {
 
         if (result == null) {
             response.setStatus(HttpStatus.NOT_FOUND_404);
-        } else {
-            // Blank out the password hash.
-            // Not strictly necessary, but sensible.
-            result.passwordHash = null;
         }
 
-        return result;
+        return removePasswordHash(result);
     }
 
+    /**
+     * Create a new user. The user will be created as inactive and no password will be set. Use {@link Password} to complete account setup.
+     *
+     * @param request Should contain a {@link User} Json message for the account details.
+     * @param response <ul>
+     *                      <li>The {@link User} that was created.</li>
+     *                      <li>if the user (i.e. email) already exists:  {@link HttpStatus#CONFLICT_409}</li>
+     *                      <li>if the user can't be created:  {@link HttpStatus#BAD_REQUEST_400}</li>
+     *                      </ul>
+     * @param user The {@link User} Json message for the account details.
+     * @return The {@link User} that was created.
+     * @throws IOException
+     */
     @POST
     public User create(HttpServletRequest request, HttpServletResponse response, User user) throws IOException {
 
@@ -55,33 +73,51 @@ public class Users {
             response.setStatus(HttpStatus.BAD_REQUEST_400);
         }
 
-        return created;
+        return removePasswordHash(created);
     }
 
+    /**
+     * Update details of a user account. NB it's not currently possible to change a user's email address.
+     * @param request Should contain a {@link User} Json message for the account details.
+     * @param response <ul>
+     *                      <li>The {@link User} that was updated.</li>
+     *                      <li>if the user (i.e. email) doesn't exist:  {@link HttpStatus#NOT_FOUND_404}</li>
+     *                      <li>if the user can't be updated: {@link HttpStatus#BAD_REQUEST_400}</li>
+     *                      </ul>
+     * @param user The {@link User} Json message containing updated account details.
+     * @return The {@link User} that was updated.
+     * @throws IOException
+     */
     @PUT
     public User update(HttpServletRequest request, HttpServletResponse response, User user) throws IOException {
-        User result = null;
 
-        result = Root.zebedee.users.update(user);
+        if (!Root.zebedee.users.exists(user)) {
+            response.setStatus(HttpStatus.NOT_FOUND_404);
+            return null;
+        }
+
+        User updated = null;
+
+        updated = Root.zebedee.users.update(user);
 
         // We'll allow changing the email at some point.
         // It entails renaming the json file and checking
         // that the new email doesn't already exist.
 
-        return result;
-    }
-
-    private String computerSaysNo(HttpServletResponse response, String message) {
-        response.setStatus(HttpStatus.UNAUTHORIZED_401);
-        return message;
-    }
-
-
-    private boolean checkCredentials(Credentials credentials, HttpServletResponse response) {
-        if (credentials == null || StringUtils.isBlank(credentials.email)) {
+        if (updated == null) {
             response.setStatus(HttpStatus.BAD_REQUEST_400);
-            return false;
         }
-        return true;
+
+
+        return removePasswordHash(updated);
+    }
+
+    private User removePasswordHash(User user) {
+        if (user != null) {
+            // Blank out the password hash.
+            // Not strictly necessary, but sensible.
+            user.passwordHash = null;
+        }
+        return user;
     }
 }

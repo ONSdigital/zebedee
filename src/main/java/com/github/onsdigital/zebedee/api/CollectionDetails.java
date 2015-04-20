90fa4b7c914e67d826760b548d8629d76e8d0320
CarlHembrough <carlhuk@gmail.com>
2015-04-20 14:12:31 +0100

    Added collection detail api method. Reads files in a collection and also reads the name property from the files to save the client having to pull all the content to read just the name.

---
+++ src/main/java/com/github/onsdigital/zebedee/api/CollectionDetails.java
@@ -0,0 +1,47 @@
+package com.github.onsdigital.zebedee.api;
+
+import com.github.davidcarboni.restolino.framework.Api;
+import com.github.onsdigital.zebedee.json.CollectionDetail;
+import org.eclipse.jetty.http.HttpStatus;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import java.io.IOException;
+
+@Api
+public class CollectionDetails {
+
+    /**
+     * Retrieves a CollectionDetail object at the endpoint /CollectionDetails/[CollectionName]
+     *
+     * @param request  This should contain a X-Florence-Token header for the current session
+     * @param response <ul>
+     *                 <li>If no collection exists:  {@link HttpStatus#NOT_FOUND_404}</li>
+     *                 </ul>
+     * @return the CollectionDetail.
+     * @throws IOException
+     */
+    @GET
+    public CollectionDetail get(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        com.github.onsdigital.zebedee.model.Collection collection = Collections
+                .getCollection(request);
+
+        if (collection == null) {
+            response.setStatus(HttpStatus.NOT_FOUND_404);
+            return null;
+        }
+
+        CollectionDetail result = new CollectionDetail();
+        result.id = collection.description.id;
+        result.name = collection.description.name;
+        result.publishDate = collection.description.publishDate;
+        result.inProgress = collection.inProgress.details();
+        result.complete = collection.complete.details();
+        result.reviewed = collection.reviewed.details();
+        result.approvedStatus = collection.description.approvedStatus;
+        return result;
+    }
+}
---
+++ src/main/java/com/github/onsdigital/zebedee/json/CollectionDetail.java
@@ -0,0 +1,27 @@
+package com.github.onsdigital.zebedee.json;
+
+import com.github.onsdigital.zebedee.model.Collection;
+
+import java.util.Date;
+import java.util.List;
+
+public class CollectionDetail {
+    /**
+     * The unique identifier of this {@link Collection}.
+     */
+    public String id;
+    /**
+     * The readable name of this {@link Collection}.
+     */
+    public String name;
+    /**
+     * The date-time when this {@link Collection} should be published (if it has
+     * a publish date).
+     */
+    public Date publishDate;
+
+    public List<ContentDetail> inProgress;
+    public List<ContentDetail> complete;
+    public List<ContentDetail> reviewed;
+    public boolean approvedStatus;
+}

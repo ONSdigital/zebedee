4d6c4ed3e8af8ea9fc259e953153148501ea22f2
CarlHembrough <carlhuk@gmail.com>
2015-04-21 14:53:27 +0100

    Preventing directories from being returned in list.

--- src/main/java/com/github/onsdigital/zebedee/model/Content.java
+++ src/main/java/com/github/onsdigital/zebedee/model/Content.java
@@ -149,17 +149,16 @@ public class Content {
      * @throws IOException
      */
     private void listFiles(Path path, List<Path> files, String glob) throws IOException {
-
-        // find the files matching the given glob.
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(path, glob)) {
             for (Path entry : stream) {
-                Path relative = this.path.relativize(entry);
-                if (!relative.endsWith(".DS_Store")) // issue when in development on Mac's
-                    files.add(relative);
+                if (!Files.isDirectory(entry)) {
+                    Path relative = this.path.relativize(entry);
+                    if (!relative.endsWith(".DS_Store")) // issue when in development on Mac's
+                        files.add(relative);
+                }
             }
         }
 
-        // recurse on any directories found.
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
             for (Path entry : stream) {
                 if (Files.isDirectory(entry)) {

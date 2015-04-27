a3e99bf75b794ee4d8d90f34ad46626c400256eb
CarlHembrough <carlhuk@gmail.com>
2015-04-27 14:45:52 +0100

    Testing of automation testing scripts.

--- run-automated-tests.sh
+++ run-automated-tests.sh
@@ -39,9 +39,9 @@ function run_tests {
 
 # update all projects concurrently with & wait
 #update_branch https://github.com/ONSdigital/florence.git $FLORENCE_DIRECTORY develop &
-update_branch https://github.com/Carboni/zebedee.git $ZEBEDEE_DIRECTORY develop &
+update_branch https://github.com/Carboni/zebedee.git $ZEBEDEE_DIRECTORY develop
 #update_branch https://github.com/ONSdigital/tredegar.git $TREDEGAR_DIRECTORY develop
-wait
+#wait
 
 #$FLORENCE_DIRECTORY/run.sh &
 $ZEBEDEE_DIRECTORY/files.sh &

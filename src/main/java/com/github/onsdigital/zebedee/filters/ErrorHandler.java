5a1294da406642dfe10c8b44a1856d547664e27b
David Carboni <david@carboni.io>
2015-04-28 10:36:00 +0100

    Simplified returning a message in the error handler.

--- src/main/java/com/github/onsdigital/zebedee/filters/ErrorHandler.java
+++ src/main/java/com/github/onsdigital/zebedee/filters/ErrorHandler.java
@@ -6,31 +6,22 @@ import com.github.onsdigital.zebedee.exceptions.ApiException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * The error handler catches various exceptions and sets the response status code accordingly.
  */
 public class ErrorHandler implements ServerError {
     @Override
-    public Object handle(HttpServletRequest req, HttpServletResponse res, RequestHandler requestHandler, Throwable t) {
+    public String handle(HttpServletRequest req, HttpServletResponse res, RequestHandler requestHandler, Throwable t) {
 
+        // If it's an ApiException subclass, set the status code and message
         if (t != null && ApiException.class.isAssignableFrom(t.getClass())) {
-
             ApiException exception = (ApiException) t;
             res.setStatus(exception.statusCode);
-
-            // Attempt to write out the exception message:
-            try {
-                PrintWriter writer = res.getWriter();
-                writer.write(exception.getMessage());
-            } catch (IOException e) {
-                // Ignore - the response may have already been committed (or something else).
-            }
+            return exception.getMessage();
         }
 
-        // Otherwise leave the default 500 response code in place.
+        // Otherwise leave the default 500 response
         return null;
     }
 }

c4f780a40d6dd37914b607668149ec1527af5c62
Thomas Ridd <twridd@gmail.com>
2015-04-30 18:30:18 +0100

    work from home

--- src/main/java/com/github/onsdigital/zebedee/api/Review.java
+++ src/main/java/com/github/onsdigital/zebedee/api/Review.java
@@ -2,6 +2,7 @@ package com.github.onsdigital.zebedee.api;
 
 import com.github.davidcarboni.restolino.framework.Api;
 import com.github.onsdigital.zebedee.exceptions.BadRequestException;
+import com.github.onsdigital.zebedee.exceptions.UnauthorizedException;
 import com.github.onsdigital.zebedee.json.ResultMessage;
 import com.github.onsdigital.zebedee.json.Session;
 import org.eclipse.jetty.http.HttpStatus;
@@ -28,7 +29,7 @@ public class Review {
      * @throws IOException
      */
     @POST
-    public ResultMessage review(HttpServletRequest request, HttpServletResponse response) throws IOException, BadRequestException {
+    public ResultMessage review(HttpServletRequest request, HttpServletResponse response) throws IOException, BadRequestException, UnauthorizedException {
 
         // Locate the collection:
         com.github.onsdigital.zebedee.model.Collection collection = Collections.getCollection(request);

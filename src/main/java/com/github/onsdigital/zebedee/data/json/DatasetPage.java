6a2833f47d556c3ac6d1a245fe32126e9e2fdf87
Thomas Ridd <twridd@gmail.com>
2015-06-08 11:31:09 +0100

    created original data publishing classes. Objects are permanent to be used in communicating with Brian. Pages will be updated when Babbage has a permanent object library

---
+++ src/main/java/com/github/onsdigital/zebedee/data/json/DatasetPage.java
@@ -0,0 +1,8 @@
+package com.github.onsdigital.zebedee.data.json;
+
+/**
+ * Created by thomasridd on 05/06/15.
+ */
+public class DatasetPage {
+    public String uri;
+}
---
+++ src/main/java/com/github/onsdigital/zebedee/data/json/TimeSeriesObjects.java
@@ -0,0 +1,9 @@
+package com.github.onsdigital.zebedee.data.json;
+
+import java.util.ArrayList;
+
+/**
+ * Created by thomasridd on 05/06/15.
+ */
+public class TimeSeriesObjects extends ArrayList<TimeSeriesObject> {
+}

31c394933c14be50bc9ecf9f089b0f37a74f2a81
Bren <brneser@gmail.com>
2015-07-30 18:30:20 +0100

    Zebedeereader data endpoint progress

--- zebedee-reader/src/main/java/com/github/onsdigital/zebedee/reader/ZebedeeReader.java
+++ zebedee-reader/src/main/java/com/github/onsdigital/zebedee/reader/ZebedeeReader.java
@@ -1,15 +1,47 @@
 package com.github.onsdigital.zebedee.reader;
 
+import com.github.onsdigital.zebedee.content.base.Content;
+import com.github.onsdigital.zebedee.exceptions.ZebedeeException;
 import com.github.onsdigital.zebedee.reader.configuration.ReaderConfiguration;
 import com.github.onsdigital.zebedee.util.ContentReader;
 
+import java.io.IOException;
+
 /**
  * Created by bren on 29/07/15.
- *
  */
 public class ZebedeeReader {
-    private static ContentReader publishedReader = new ContentReader(ReaderConfiguration.getZebedeeRoot() + "/master") ;
+    private static ZebedeeReader instance;
+
+    private static ContentReader publishedContentReader;
+
+    //Singleton
+    private ZebedeeReader() {
+        publishedContentReader = new ContentReader(ReaderConfiguration.getPublishedFolder());
+    }
+
+    public static ZebedeeReader getInstance() {
+        if (instance == null) {
+            synchronized (ZebedeeReader.class) {
+                if (instance == null) {
+                    instance = new ZebedeeReader();
+                }
+            }
+        }
+        return instance;
+    }
 
 
+    public Content getPublishedContent(String path) throws ZebedeeException, IOException {
+        return publishedContentReader.getContent(path);
+    }
+
+    public Content getCollectionContent(String path) throws ZebedeeException, IOException {
+        return null;
+    }
+
+    public Resource getPublishedResource(String path) throws ZebedeeException, IOException {
+        return null;
+    }
 
 }
--- zebedee-reader/src/main/java/com/github/onsdigital/zebedee/reader/configuration/ReaderConfiguration.java
+++ zebedee-reader/src/main/java/com/github/onsdigital/zebedee/reader/configuration/ReaderConfiguration.java
@@ -9,12 +9,15 @@ import org.apache.commons.lang3.StringUtils;
  * Content reader configuration
  */
 public class ReaderConfiguration {
-    private final static String ZEBEDEE_ROOT =  "ZEBEDEE_ROOT";
+    private final static String ZEBEDEE_ROOT = "ZEBEDEE_ROOT";
     private static final String DEFAULT_ZEBEDEE_ROOT = "zebedee-reader/target/zebedee";
 
     private static final String IN_PROGRESS_FOLDER_NAME = "inprogress";
     private static final String COMPLETE_FOLDER_NAME = "complete";
     private static final String REVIEWED_FOLDER_NAME = "reviewed";
+    private static final String COLLECTIONS_FOLDER_NAME = "collections";
+    private static final String PUBLISHED_FOLDER = "master";
+    private static String collectionsFolder;
 
     public static String getZebedeeRoot() {
         return StringUtils.defaultIfBlank(VariableUtils.getVariableValue(ZEBEDEE_ROOT), DEFAULT_ZEBEDEE_ROOT);
@@ -30,5 +33,28 @@ public class ReaderConfiguration {
 
     public static String getReviewedFolderName() {
         return REVIEWED_FOLDER_NAME;
+    }
+
+    public static String getPublishedFolder() {
+        String zebedeeRoot = getZebedeeRoot();
+        String publishedFolder = "";
+        if (zebedeeRoot != null) {
+            publishedFolder += zebedeeRoot.endsWith("/") ? PUBLISHED_FOLDER : "/" + PUBLISHED_FOLDER;
+        }
+        return publishedFolder;
+    }
+
+    public static String getCollectionsFolder() {
+        if (collectionsFolder == null) {
+            synchronized (collectionsFolder) {
+                String zebedeeRoot = getZebedeeRoot();
+                String collectionsFolder = "";
+                if (zebedeeRoot != null) {
+                    collectionsFolder += zebedeeRoot.endsWith("/") ? COLLECTIONS_FOLDER_NAME : "/" + COLLECTIONS_FOLDER_NAME;
+                }
+            }
+        }
+
+        return collectionsFolder;
     }
 }

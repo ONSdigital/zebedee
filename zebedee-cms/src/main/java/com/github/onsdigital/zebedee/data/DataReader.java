99563aa2dc092068f50e08d2e99df1c51d54ac22
CarlHembrough <carlhuk@gmail.com>
2015-07-30 17:31:09 +0100

    Make resolve aware of /latest uri's.

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/data/DataReader.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/data/DataReader.java
@@ -3,6 +3,7 @@ package com.github.onsdigital.zebedee.data;
 import com.github.onsdigital.content.DirectoryListing;
 import com.github.onsdigital.content.service.ContentNotFoundException;
 import com.github.onsdigital.content.service.ContentService;
+import com.github.onsdigital.content.util.URIUtil;
 import com.github.onsdigital.zebedee.api.Root;
 import com.github.onsdigital.zebedee.exceptions.BadRequestException;
 import com.github.onsdigital.zebedee.exceptions.NotFoundException;
@@ -11,10 +12,15 @@ import com.github.onsdigital.zebedee.json.Session;
 import com.github.onsdigital.zebedee.model.Collection;
 import org.apache.commons.lang.StringUtils;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by bren on 13/06/15.
@@ -33,6 +39,18 @@ public class DataReader implements ContentService {
 
     @Override
     public InputStream readData(String uri) throws ContentNotFoundException {
+
+        // if the uri is /latest then resolve the uri to the latest.
+        String requestType = URIUtil.resolveRequestType(uri);
+        if (requestType.equals("latest")) {
+            uri = URIUtil.resolveResouceUri(uri);
+            DirectoryListing listing = readDirectory(uri);
+            List<String> folders = new ArrayList<>(listing.folders.keySet());
+            Collections.sort(folders, Collections.reverseOrder());
+            String release = new File(folders.get(0)).getName();
+            uri = Paths.get(uri).resolve(release).toString();
+        }
+
         try {
             return getDataStream(uri);
         }  catch (IOException e) {
@@ -63,7 +81,5 @@ public class DataReader implements ContentService {
         } else {
             return Files.newInputStream(dataPath);
         }
-
     }
-
 }

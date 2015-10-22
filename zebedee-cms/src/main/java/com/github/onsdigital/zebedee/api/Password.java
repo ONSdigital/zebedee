a23a697d3e95b3dca1c2badff3d9c173ca2bac15
David Carboni <david@carboni.io>
2015-10-22 18:38:16 +0100

    Changed to authenticate directly on User rather than via the Users model class.

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/api/Password.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/api/Password.java
@@ -2,10 +2,11 @@ package com.github.onsdigital.zebedee.api;
 
 import com.github.davidcarboni.restolino.framework.Api;
 import com.github.onsdigital.zebedee.exceptions.BadRequestException;
+import com.github.onsdigital.zebedee.exceptions.NotFoundException;
 import com.github.onsdigital.zebedee.exceptions.UnauthorizedException;
 import com.github.onsdigital.zebedee.json.Credentials;
 import com.github.onsdigital.zebedee.json.Session;
-import org.apache.commons.lang3.StringUtils;
+import com.github.onsdigital.zebedee.json.User;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,7 +14,7 @@ import javax.ws.rs.POST;
 import java.io.IOException;
 
 /**
- * Created by david on 12/03/2015.
+ * API for resetting or changing a password.
  */
 @Api
 public class Password {
@@ -24,7 +25,7 @@ public class Password {
      * Will set password as permanent if it is the user updating
      * Will set password as temporary if an admin does it
      *
-     * @param request - a session with appropriate permissions
+     * @param request     - a session with appropriate permissions
      * @param response
      * @param credentials - new credentials
      * @return
@@ -33,22 +34,24 @@ public class Password {
      * @throws BadRequestException
      */
     @POST
-    public String setPassword(HttpServletRequest request, HttpServletResponse response, Credentials credentials) throws IOException, UnauthorizedException, BadRequestException {
-        // Check the user session
+    public String setPassword(HttpServletRequest request, HttpServletResponse response, Credentials credentials) throws IOException, UnauthorizedException, BadRequestException, NotFoundException {
+
+        // Get the user session
         Session session = Root.zebedee.sessions.get(request);
 
-        // see if the user is resetting their password: authenticate with old password
-        if (session == null && StringUtils.isNotEmpty(credentials.oldPassword)) {
-            boolean authenticated = Root.zebedee.users.authenticate(credentials.email, credentials.oldPassword);
-
-            // if we can authenticate with the old password we know we are safe to update the password
-            if (authenticated) {
+        // If the user is not logged in, but they are attempting to change their password, authenticate using the old password
+        if (session == null && credentials != null) {
+            User user = Root.zebedee.users.get(credentials.email);
+            if (user.authenticate(credentials.oldPassword)) {
                 session = Root.zebedee.sessions.create(credentials.email);
             }
         }
 
-        Root.zebedee.users.setPassword(session, credentials);
-
-        return "Password updated for " + credentials.email;
+        // Attempt to change or reset the password:
+        if (Root.zebedee.users.setPassword(session, credentials)) {
+            return "Password updated for " + credentials.email;
+        } else {
+            return "Password not updated for " + credentials.email + " (there may be an issue with the user's keyring password).";
+        }
     }
 }

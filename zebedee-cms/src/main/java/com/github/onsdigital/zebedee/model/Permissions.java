4acbf9060c04cef8f4bfa3a214ae5d25579d3115
Thomas Ridd <twridd@gmail.com>
2015-11-25 16:14:12 +0000

    Overloaded canEdit and canView functions

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/model/Permissions.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/model/Permissions.java
@@ -122,7 +122,7 @@ public class Permissions {
     }
 
     /**
-     * Determines whether the specified user has editing rights.
+     * Responds only on the basis of whether a user is an editor
      *
      * @param email The user's email.
      * @return If the user is a member of the Digital Publishing team, true.
@@ -144,7 +144,11 @@ public class Permissions {
      * @throws IOException
      */
     public boolean canEdit(Session session, CollectionDescription collectionDescription) throws IOException {
-        return canEdit(session.email) && zebedee.keyringCache.get(session).list().contains(collectionDescription.id);
+        if (collectionDescription.isEncrypted) {
+            return canEdit(session.email) && zebedee.keyringCache.get(session).list().contains(collectionDescription.id);
+        } else {
+            return canEdit(session.email);
+        }
     }
 
     /**
@@ -156,7 +160,28 @@ public class Permissions {
      * @throws IOException
      */
     public boolean canEdit(User user, CollectionDescription collectionDescription) throws IOException {
-        return canEdit(user.email) && user.keyring.list().contains(collectionDescription.id);
+        if (collectionDescription.isEncrypted) {
+            return canEdit(user.email) && user.keyring.list().contains(collectionDescription.id);
+        } else {
+            return canEdit(user.email);
+        }
+    }
+
+
+    /**
+     * Get whether a user can edit a collection
+     *
+     * @param email the user email
+     * @param collectionDescription
+     * @return
+     * @throws IOException
+     */
+    public boolean canEdit(String email, CollectionDescription collectionDescription) throws IOException {
+        try {
+            return canEdit(zebedee.users.get(email), collectionDescription);
+        } catch (BadRequestException | NotFoundException e) {
+            return false;
+        }
     }
 
 
@@ -210,6 +235,9 @@ public class Permissions {
     /**
      * Determines whether a session has viewing rights.
      *
+     * If a collection has its encrypted flag set returns yes for any user with the key
+     * If not encrypted only gives view permission to publisher/admins
+     *
      * @param session               The user's session. Can be null.
      * @param collectionDescription The collection to check access for.
      * @return True if the user is a member of the Digital Publishing team or
@@ -217,11 +245,18 @@ public class Permissions {
      * @throws IOException If a filesystem error occurs.
      */
     public boolean canView(Session session, CollectionDescription collectionDescription) throws IOException {
-        return session != null && zebedee.keyringCache.get(session).list().contains(collectionDescription.id);
+        if (collectionDescription.isEncrypted) {
+            return session != null && zebedee.keyringCache.get(session).list().contains(collectionDescription.id);
+        } else {
+            return session != null && canEdit(session);
+        }
     }
 
     /**
      * Determines whether the specified user has viewing rights.
+     *
+     * If a collection has its encrypted flag set returns yes for any user with the key
+     * If not encrypted only gives view permission to publisher/admins
      *
      * @param user               The user. Can be null.
      * @param collectionDescription The collection to check access for.
@@ -230,7 +265,29 @@ public class Permissions {
      * @throws IOException If a filesystem error occurs.
      */
     public boolean canView(User user, CollectionDescription collectionDescription) throws IOException {
-        return user != null && user.keyring.list().contains(collectionDescription.id);
+        if (collectionDescription.isEncrypted) {
+            return user != null && user.keyring.list().contains(collectionDescription.id);
+        } else {
+            return user != null && canEdit(user.email);
+        }
+    }
+
+    /**
+     * Determines whether the specified user has viewing rights.
+     *
+     * @param email               The email of the user
+     * @param collectionDescription The collection to check access for.
+     * @return True if the user is a member of the Digital Publishing team or
+     * the user is a content owner with access to the given path or any parent path.
+     * @throws IOException If a filesystem error occurs.
+     */
+    public boolean canView(String email, CollectionDescription collectionDescription) throws IOException {
+
+        try {
+            return canView(zebedee.users.get(email), collectionDescription);
+        } catch (NotFoundException | BadRequestException e) {
+            return false;
+        }
     }
 
     /**

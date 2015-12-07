c99c2895666f114dc70e3960fd5da0d0364d60af
CarlHembrough <carlhuk@gmail.com>
2015-12-07 16:23:28 +0000

    Basic implementation of ContentWriter.

---
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/model/ContentWriter.java
@@ -0,0 +1,70 @@
+package com.github.onsdigital.zebedee.model;
+
+import com.github.onsdigital.zebedee.exceptions.BadRequestException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static com.github.onsdigital.zebedee.util.URIUtils.removeLeadingSlash;
+import static java.nio.file.Files.isDirectory;
+
+/**
+ * Basic abstraction for writing content to file.
+ */
+public class ContentWriter {
+
+    private final Path ROOT_FOLDER;
+
+    /**
+     * Create a new instance of ContentWriter to write content from the given root folder.
+     *
+     * @param rootFolder
+     */
+    public ContentWriter(Path rootFolder) {
+        if (rootFolder == null) {
+            throw new NullPointerException("Root folder can not be null");
+        }
+        this.ROOT_FOLDER = rootFolder;
+    }
+
+    /**
+     * Write the given input stream to the given URI.
+     *
+     * @param input
+     * @param uri
+     * @throws BadRequestException
+     * @throws IOException
+     */
+    public void write(InputStream input, String uri) throws BadRequestException, IOException {
+        Path path = resolvePath(uri);
+        write(input, path);
+    }
+
+    protected void write(InputStream input, Path path) throws BadRequestException, IOException {
+        assertNotDirectory(path);
+        try (OutputStream output = Files.newOutputStream(path)) {
+            org.apache.commons.io.IOUtils.copy(input, output);
+        }
+    }
+
+    private void assertNotDirectory(Path path) throws BadRequestException {
+        if (isDirectory(path)) {
+            throw new BadRequestException("Requested path is a directory");
+        }
+    }
+
+    private Path resolvePath(String path) throws BadRequestException {
+        if (path == null) {
+            throw new NullPointerException("Path can not be null");
+        }
+        return getRootFolder().resolve(removeLeadingSlash(path));
+    }
+
+    private Path getRootFolder() {
+        return ROOT_FOLDER;
+    }
+
+}

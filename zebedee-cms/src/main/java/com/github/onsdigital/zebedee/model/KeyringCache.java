832ca180fc8d7a7072cb81592519bfd941021fa2
CarlHembrough <carlhuk@gmail.com>
2015-12-08 12:17:57 +0000

    Pass session in to prevent having to read it again.

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/model/KeyringCache.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/model/KeyringCache.java
@@ -15,12 +15,9 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class KeyringCache {
 
-    private  Map<Session, Keyring> keyringMap = new ConcurrentHashMap<>();
-
     // Publisher keyring keeps all available secret keys available
     public Map<String, SecretKey> schedulerCache = new ConcurrentHashMap<>();
-
-
+    private Map<Session, Keyring> keyringMap = new ConcurrentHashMap<>();
     private Zebedee zebedee;
 
     public KeyringCache(Zebedee zebedee) {
@@ -28,23 +25,20 @@ public class KeyringCache {
         this.zebedee = zebedee;
     }
 
-
-
     /**
      * Stores the specified user's keyring, if unlocked, in the cache.
      *
      * @param user The user whose {@link Keyring} is to be stored.
      * @throws IOException If a general error occurs.
      */
-    public  void put(User user) throws IOException {
+    public void put(User user, Session session) throws IOException {
         if (user != null && user.keyring() != null && user.keyring().isUnlocked()) {
-            Session session = zebedee.sessions.find(user.email);
             if (session != null) {
                 // add the keyring by session
                 keyringMap.put(session, user.keyring());
 
                 // populate the scheduler keyring
-                for (String collectionId: user.keyring.list()) {
+                for (String collectionId : user.keyring.list()) {
                     schedulerCache.put(collectionId, user.keyring.get(collectionId));
                 }
             }
@@ -58,7 +52,7 @@ public class KeyringCache {
      * @return The {@link Keyring} if present, or null.
      * @throws IOException If a general error occurs.
      */
-    public  Keyring get(User user) throws IOException {
+    public Keyring get(User user) throws IOException {
         Keyring result = null;
 
         if (user != null) {
@@ -78,7 +72,7 @@ public class KeyringCache {
      * @return The {@link Keyring} if present, or null.
      * @throws IOException If a general error occurs.
      */
-    public  Keyring get(Session session) throws IOException {
+    public Keyring get(Session session) throws IOException {
         Keyring result = null;
 
         if (session != null) {
@@ -93,7 +87,7 @@ public class KeyringCache {
      * @param session The expired {@link Session} for which the {@link Keyring} is to be removed.
      * @throws IOException If a general error occurs.
      */
-    public  void remove(Session session) throws IOException {
+    public void remove(Session session) throws IOException {
         if (session != null) {
             keyringMap.remove(session);
         }

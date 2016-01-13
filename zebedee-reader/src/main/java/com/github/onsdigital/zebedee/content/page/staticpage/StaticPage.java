f2cdf157561124b1bb3ffc87c1fba2d85c206eb2
pastor-methods <pastor.methods@gmail.com>
2016-01-13 12:32:40 +0000

    Add charts, tables and images to static pages

--- zebedee-reader/src/main/java/com/github/onsdigital/zebedee/content/page/staticpage/StaticPage.java
+++ zebedee-reader/src/main/java/com/github/onsdigital/zebedee/content/page/staticpage/StaticPage.java
@@ -2,6 +2,10 @@ package com.github.onsdigital.zebedee.content.page.staticpage;
 
 import com.github.onsdigital.zebedee.content.page.base.PageType;
 import com.github.onsdigital.zebedee.content.page.staticpage.base.BaseStaticPage;
+import com.github.onsdigital.zebedee.content.page.statistics.document.figure.FigureSection;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by bren on 30/06/15.
@@ -9,6 +13,36 @@ import com.github.onsdigital.zebedee.content.page.staticpage.base.BaseStaticPage
  * Simple static page with only markdown content
  */
 public class StaticPage extends BaseStaticPage {
+
+    private List<FigureSection> charts = new ArrayList<>();
+    private List<FigureSection> tables = new ArrayList<>();
+    private List<FigureSection> images = new ArrayList<>();
+
+    public List<FigureSection> getCharts() {
+        return charts;
+    }
+
+    public void setCharts(List<FigureSection> charts) {
+        this.charts = charts;
+    }
+
+    public List<FigureSection> getTables() {
+        return tables;
+    }
+
+    public void setTables(List<FigureSection> tables) {
+        this.tables = tables;
+    }
+
+    public List<FigureSection> getImages() {
+        return images;
+    }
+
+    public void setImages(List<FigureSection> images) {
+        this.images = images;
+    }
+
+
     @Override
     public PageType getType() {
         return PageType.static_page;

70cdbd2dd6c22f47d067eaa34a5a4eb0c985203a
CarlHembrough <carlhuk@gmail.com>
2016-01-26 10:46:21 +0000

    Renamed Scheduler to RunnableScheduler.

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/model/publishing/scheduled/Scheduler.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/model/publishing/scheduled/Scheduler.java
@@ -1,49 +1,29 @@
 package com.github.onsdigital.zebedee.model.publishing.scheduled;
 
+import com.github.onsdigital.zebedee.Zebedee;
+import com.github.onsdigital.zebedee.configuration.Configuration;
+import com.github.onsdigital.zebedee.json.CollectionType;
+import com.github.onsdigital.zebedee.model.Collection;
 import com.github.onsdigital.zebedee.util.Log;
 
-import java.util.Date;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+public abstract class Scheduler {
 
-/**
- * Schedule a task to run at the given date.
- */
-public class Scheduler {
-
-    ScheduledExecutorService scheduledExecutorService;
-
-    public Scheduler() {
-        this(10); // default thread pool size of 10;
+    public void schedulePublish(Collection collection, Zebedee zebedee) {
+        if (Configuration.isSchedulingEnabled()) {
+            try {
+                System.out.println("Attempting to schedule publish for collection " + collection.description.name + " type=" + collection.description.type);
+                if (collection.description.type == CollectionType.scheduled) {
+                    schedule(collection, zebedee);
+                }
+            } catch (Exception e) {
+                System.out.println("Exception caught trying to schedule existing collection: " + e.getMessage());
+            }
+        } else {
+            Log.print("Not scheduling collection %s, scheduling is not enabled", collection.description.name);
+        }
     }
 
-    public Scheduler(int poolSize) {
-        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(poolSize);
-        executor.setRemoveOnCancelPolicy(true);
-        this.scheduledExecutorService = executor;
-    }
+    protected abstract void schedule(Collection collection, Zebedee zebedee);
 
-    /**
-     * Schedule a task to run on the specified date.
-     *
-     * @param task
-     * @param scheduledDate
-     */
-    public ScheduledFuture<?> schedule(Runnable task, Date scheduledDate) {
-        long nowMs = System.currentTimeMillis();
-        long ms = scheduledDate.getTime() - nowMs;
-        ScheduledFuture<?> future = scheduledExecutorService.schedule(task, ms, TimeUnit.MILLISECONDS);
-        Log.print("Task scheduled to run in " + ms + "ms.");
-        return future;
-    }
-
-    /**
-     * Shutdown the scheduler to ensure resources are disposed of.
-     */
-    public void shutdown() {
-        System.out.println("Shutdown called on Scheduler.");
-        scheduledExecutorService.shutdown();
-    }
+    public abstract void cancel(Collection collection);
 }

79980124b83e5f929b55a639776a0dfd6ac1cf0c
pastor-methods <pastor.methods@gmail.com>
2016-02-04 11:12:16 +0000

    Add downloads to methodology download (pdf)

--- zebedee-reader/src/main/java/com/github/onsdigital/zebedee/content/page/staticpage/MethodologyDownload.java
+++ zebedee-reader/src/main/java/com/github/onsdigital/zebedee/content/page/staticpage/MethodologyDownload.java
@@ -2,6 +2,7 @@ package com.github.onsdigital.zebedee.content.page.staticpage;
 
 import com.github.onsdigital.zebedee.content.page.base.PageType;
 import com.github.onsdigital.zebedee.content.page.staticpage.base.BaseStaticPage;
+import com.github.onsdigital.zebedee.content.page.statistics.dataset.DownloadSection;
 import com.github.onsdigital.zebedee.content.partial.Link;
 
 import java.util.List;
@@ -10,6 +11,7 @@ public class MethodologyDownload extends BaseStaticPage {
 
     private List<Link> relatedDocuments;
     private List<Link> relatedDatasets;
+    private List<DownloadSection> pdfDownloads;
 
     public List<Link> getRelatedDocuments() {
         return relatedDocuments;
@@ -25,6 +27,14 @@ public class MethodologyDownload extends BaseStaticPage {
 
     public void setRelatedDatasets(List<Link> relatedDatasets) {
         this.relatedDatasets = relatedDatasets;
+    }
+
+    public List<DownloadSection> getPdfDownloads() {
+        return pdfDownloads;
+    }
+
+    public void setPdfDownloads(List<DownloadSection> pdfDownloads) {
+        this.pdfDownloads = pdfDownloads;
     }
 
     @Override

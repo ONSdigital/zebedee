ee246750fec7956187fe0a3515db614cb4bfbb13
Bren <brneser@gmail.com>
2016-02-05 14:21:54 +0000

    Changed elastic search client back to transport client

--- zebedee-reader/src/main/java/com/github/onsdigital/zebedee/search/client/ElasticSearchClient.java
+++ zebedee-reader/src/main/java/com/github/onsdigital/zebedee/search/client/ElasticSearchClient.java
@@ -1,5 +1,6 @@
 package com.github.onsdigital.zebedee.search.client;
 
+import com.github.onsdigital.zebedee.search.configuration.SearchConfiguration;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
@@ -72,22 +73,34 @@ public class ElasticSearchClient {
 
     private static void connect() throws IOException {
         if (client == null) {
-            searchHome = Files.createTempDirectory("zebedee_search_client");
-            Settings settings = Settings.builder().put("http.enabled", false)
-                    .put("cluster.name", getElasticSearchCluster())
-                    .put("discovery.zen.ping.multicast.enabled", true)
-                    .put("path.home", searchHome).build();
-            Node node =
-                    nodeBuilder()
-                            .settings(settings)
-                            .data(false)
-                            .node();
-
-            client = node.client();
-            Runtime.getRuntime().addShutdownHook(new ShutDownNodeThread(client, null));
+            initTransportClient();
+//            initNodeClient();
         }
+    }
 
 
+    protected static void initTransportClient() throws IOException {
+        Settings settings = Settings.builder()
+                .put("cluster.name", getElasticSearchCluster()).build();
+        client = TransportClient.builder().settings(settings).build()
+                .addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress(getElasticSearchServer(), SearchConfiguration.getElasticSearchPort())));
+    }
+
+
+    private static void initNodeClient() throws IOException {
+        searchHome = Files.createTempDirectory("zebedee_search_client");
+        Settings settings = Settings.builder().put("http.enabled", false)
+                .put("cluster.name", getElasticSearchCluster())
+                .put("discovery.zen.ping.multicast.enabled", true)
+                .put("path.home", searchHome).build();
+        Node node =
+                nodeBuilder()
+                        .settings(settings)
+                        .data(false)
+                        .node();
+
+        client = node.client();
+        Runtime.getRuntime().addShutdownHook(new ShutDownNodeThread(client, null));
     }
 
     static class ShutDownNodeThread extends Thread {

503ed3004e2ebb9421bb63b6b80c41c5eb55b342
CarlHembrough <carlhuk@gmail.com>
2016-02-09 13:58:28 +0000

    Added CsdbKey endpoint that provides the public key allowing the CSDB import process to provide an encrypted key.

---
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/api/CsdbKey.java
@@ -0,0 +1,18 @@
+package com.github.onsdigital.zebedee.api;
+
+import com.github.davidcarboni.restolino.framework.Api;
+import com.github.onsdigital.zebedee.exceptions.ZebedeeException;
+import com.github.onsdigital.zebedee.model.CsdbImporter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import java.io.IOException;
+
+@Api
+public class CsdbKey {
+    @GET
+    public String getPublicKey(HttpServletRequest request, HttpServletResponse response) throws IOException, ZebedeeException {
+        return Root.zebedee.applicationKeys.getEncodedPublicKey(CsdbImporter.APPLICATION_KEY_ID);
+    }
+}

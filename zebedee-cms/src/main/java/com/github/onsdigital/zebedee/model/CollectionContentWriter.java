cc15ef51c6293f9b7285b557c66404953307f27c
CarlHembrough <carlhuk@gmail.com>
2016-02-09 12:08:04 +0000

    Refactored CollectionWriter to allow initialisation with a SecretKey instead of a session.

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/model/CollectionContentWriter.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/model/CollectionContentWriter.java
@@ -1,10 +1,7 @@
 package com.github.onsdigital.zebedee.model;
 
-import com.github.onsdigital.zebedee.Zebedee;
 import com.github.onsdigital.zebedee.exceptions.BadRequestException;
 import com.github.onsdigital.zebedee.exceptions.UnauthorizedException;
-import com.github.onsdigital.zebedee.json.Keyring;
-import com.github.onsdigital.zebedee.json.Session;
 import com.github.onsdigital.zebedee.util.EncryptionUtils;
 import org.apache.commons.io.FileUtils;
 
@@ -17,24 +14,18 @@ import java.nio.file.Path;
  * Collection specific implementation of ContentWriter that is session / encryption aware.
  */
 public class CollectionContentWriter extends ContentWriter {
-
-    private Zebedee zebedee;
     private Collection collection;
-    private Session session;
+    private SecretKey key;
 
     /**
      * Create a new instance of ContentWriter to write content from the given root folder.
      *
      * @param rootFolder
      */
-    public CollectionContentWriter(Zebedee zebedee, Collection collection, Session session, Path rootFolder) throws IOException, UnauthorizedException {
+    public CollectionContentWriter(Collection collection, SecretKey key, Path rootFolder) throws IOException, UnauthorizedException {
         super(rootFolder);
-        this.zebedee = zebedee;
         this.collection = collection;
-        this.session = session;
-
-        Keyring keyring = zebedee.keyringCache.get(session);
-        if (keyring == null) throw new UnauthorizedException("No keyring is available for " + session.email);
+        this.key = key;
     }
 
     @Override
@@ -42,7 +33,6 @@ public class CollectionContentWriter extends ContentWriter {
         Path path = resolvePath(uri);
         assertNotDirectory(path);
         if (collection.description.isEncrypted) {
-            SecretKey key = zebedee.keyringCache.get(session).get(collection.description.id);
             return EncryptionUtils.encryptionOutputStream(path, key);
         } else {
             return FileUtils.openOutputStream(path.toFile());
--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/model/ZebedeeCollectionWriter.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/model/ZebedeeCollectionWriter.java
@@ -4,9 +4,11 @@ import com.github.onsdigital.zebedee.Zebedee;
 import com.github.onsdigital.zebedee.exceptions.BadRequestException;
 import com.github.onsdigital.zebedee.exceptions.NotFoundException;
 import com.github.onsdigital.zebedee.exceptions.UnauthorizedException;
+import com.github.onsdigital.zebedee.json.Keyring;
 import com.github.onsdigital.zebedee.json.Session;
 import com.github.onsdigital.zebedee.reader.configuration.ReaderConfiguration;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
 
 import static com.github.onsdigital.zebedee.configuration.Configuration.getUnauthorizedMessage;
@@ -29,10 +31,6 @@ public class ZebedeeCollectionWriter extends CollectionWriter {
      */
     public ZebedeeCollectionWriter(Zebedee zebedee, Collection collection, Session session) throws BadRequestException, IOException, UnauthorizedException, NotFoundException {
 
-        if (collection == null) {
-            throw new NotFoundException("Please specify a collection");
-        }
-
         // Authorisation
         if (session == null
                 || !zebedee.permissions.canEdit(session.email,
@@ -40,10 +38,37 @@ public class ZebedeeCollectionWriter extends CollectionWriter {
             throw new UnauthorizedException(getUnauthorizedMessage(session));
         }
 
+        Keyring keyring = zebedee.keyringCache.get(session);
+        if (keyring == null) throw new UnauthorizedException("No keyring is available for " + session.email);
+
+        SecretKey key = keyring.get(collection.description.id);
+        init(collection, key);
+    }
+
+    /**
+     * Create a new instance of CollectionWriter for the given SecretKey if you already have it.
+     *
+     * @param collection
+     * @param key
+     * @throws BadRequestException
+     * @throws IOException
+     * @throws UnauthorizedException
+     * @throws NotFoundException
+     */
+    public ZebedeeCollectionWriter(Collection collection, SecretKey key) throws BadRequestException, IOException, UnauthorizedException, NotFoundException {
+        init(collection, key);
+    }
+
+    private void init(Collection collection, SecretKey key) throws NotFoundException, UnauthorizedException, IOException {
+
+        if (collection == null) {
+            throw new NotFoundException("Please specify a collection");
+        }
+
         ReaderConfiguration config = getConfiguration();
 
-        inProgress = new CollectionContentWriter(zebedee, collection, session, collection.path.resolve(config.getInProgressFolderName()));
-        complete = new CollectionContentWriter(zebedee, collection, session, collection.path.resolve(config.getCompleteFolderName()));
-        reviewed = new CollectionContentWriter(zebedee, collection, session, collection.path.resolve(config.getReviewedFolderName()));
+        inProgress = new CollectionContentWriter(collection, key, collection.path.resolve(config.getInProgressFolderName()));
+        complete = new CollectionContentWriter(collection, key, collection.path.resolve(config.getCompleteFolderName()));
+        reviewed = new CollectionContentWriter(collection, key, collection.path.resolve(config.getReviewedFolderName()));
     }
 }

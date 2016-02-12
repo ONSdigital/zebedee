54b3280104cd8acaa2789adf205c0ca31d646e7c
CarlHembrough <carlhuk@gmail.com>
2016-02-12 11:57:44 +0000

    Extracted logic for compressing timeseries in a collection into its own class.

---
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/model/publishing/preprocess/TimeSeriesCompressor.java
@@ -0,0 +1,83 @@
+package com.github.onsdigital.zebedee.model.publishing.preprocess;
+
+import com.github.onsdigital.zebedee.exceptions.BadRequestException;
+import com.github.onsdigital.zebedee.exceptions.ZebedeeException;
+import com.github.onsdigital.zebedee.model.Collection;
+import com.github.onsdigital.zebedee.model.ContentWriter;
+import com.github.onsdigital.zebedee.model.content.item.VersionedContentItem;
+import com.github.onsdigital.zebedee.reader.ContentReader;
+import com.github.onsdigital.zebedee.util.Log;
+import com.github.onsdigital.zebedee.util.ZipUtils;
+import org.apache.commons.io.FileUtils;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Path;
+import java.util.List;
+
+public class TimeSeriesCompressor {
+
+    /**
+     * Compress timeseries
+     * <p>
+     * Uses content
+     *
+     * @param contentReader
+     * @param contentWriter
+     * @throws BadRequestException
+     * @throws IOException
+     */
+    public static void compressFiles(ContentReader contentReader, ContentWriter contentWriter, Collection collection) throws ZebedeeException, IOException {
+        Log.print("Compressing time series directories...");
+
+        List<Path> timeSeriesDirectories = contentReader.listTimeSeriesDirectories();
+
+        for (Path timeSeriesDirectory : timeSeriesDirectories) {
+            Log.print("Compressing time series directory %s", timeSeriesDirectory.toString());
+            String saveUri = contentReader.getRootFolder().relativize(timeSeriesDirectory).toString() + "-to-publish.zip";
+
+            if (!collection.description.isEncrypted) {
+                try (OutputStream outputStream = contentWriter.getOutputStream(saveUri)) {
+                    ZipUtils.zipFolder(timeSeriesDirectory.toFile(), outputStream,
+                            url -> VersionedContentItem.isVersionedUri(url));
+                }
+            } else {
+                ZipUtils.zipFolderWithEncryption(contentReader, contentWriter, timeSeriesDirectory.toFile().toString(), saveUri,
+                        url -> VersionedContentItem.isVersionedUri(url));
+            }
+            Log.print("Deleting directory after compression %s", timeSeriesDirectory);
+            FileUtils.deleteDirectory(timeSeriesDirectory.toFile());
+        }
+    }
+
+    //    /**
+//     * Zip up timeseries to be transferred by the train
+//     *
+//     * @param collection the collection being published
+//     * @throws IOException
+//     */
+//    public static void compressTimeseries(Zebedee zebedee, Collection collection) throws IOException {
+//        Log.print("Compressing time series directories...");
+//        List<Path> timeSeriesDirectories = collection.reviewed.listTimeSeriesDirectories();
+//        for (Path timeSeriesDirectory : timeSeriesDirectories) {
+//
+//            Log.print("Compressing time series directory %s", timeSeriesDirectory.toString());
+//            if (collection.description.isEncrypted) {
+//                ZipUtils.zipFolderWithEncryption(
+//                        timeSeriesDirectory.toFile(),
+//                        new File(timeSeriesDirectory.toString() + "-to-publish.zip"),
+//                        zebedee.keyringCache.schedulerCache.get(collection.description.id),
+//                        url -> VersionedContentItem.isVersionedUri(url));
+//            } else {
+//                ZipUtils.zipFolder(
+//                        timeSeriesDirectory.toFile(),
+//                        new File(timeSeriesDirectory.toString() + "-to-publish.zip"),
+//                        url -> VersionedContentItem.isVersionedUri(url));
+//            }
+//
+//            Log.print("Deleting directory after compression %s", timeSeriesDirectory);
+//            FileUtils.deleteDirectory(timeSeriesDirectory.toFile());
+//        }
+//    }
+
+}

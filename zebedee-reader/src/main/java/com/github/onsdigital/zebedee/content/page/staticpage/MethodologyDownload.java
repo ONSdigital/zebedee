401027ff209f465bd7af098870fe63a26104e6ad
Crispioso <crispin.merriman@gmail.com>
2016-02-15 11:56:11 +0000

    Add alerts to static methodology download page

--- zebedee-reader/src/main/java/com/github/onsdigital/zebedee/content/page/staticpage/MethodologyDownload.java
+++ zebedee-reader/src/main/java/com/github/onsdigital/zebedee/content/page/staticpage/MethodologyDownload.java
@@ -3,6 +3,7 @@ package com.github.onsdigital.zebedee.content.page.staticpage;
 import com.github.onsdigital.zebedee.content.page.base.PageType;
 import com.github.onsdigital.zebedee.content.page.staticpage.base.BaseStaticPage;
 import com.github.onsdigital.zebedee.content.page.statistics.dataset.DownloadSection;
+import com.github.onsdigital.zebedee.content.partial.Alert;
 import com.github.onsdigital.zebedee.content.partial.Link;
 
 import java.util.List;
@@ -12,6 +13,8 @@ public class MethodologyDownload extends BaseStaticPage {
     private List<Link> relatedDocuments;
     private List<Link> relatedDatasets;
     private List<DownloadSection> pdfDownloads;
+
+    private List<Alert> alerts;
 
     public List<Link> getRelatedDocuments() {
         return relatedDocuments;
@@ -36,6 +39,10 @@ public class MethodologyDownload extends BaseStaticPage {
     public void setPdfDownloads(List<DownloadSection> pdfDownloads) {
         this.pdfDownloads = pdfDownloads;
     }
+
+    public List<Alert> getAlerts() { return alerts; }
+
+    public void setAlerts(List<Alert> alerts) { this.alerts = alerts; }
 
     @Override
     public PageType getType() {

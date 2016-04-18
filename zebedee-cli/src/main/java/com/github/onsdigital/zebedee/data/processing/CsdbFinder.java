75b9168b51e36bfb48e20e5d8bba9f216e331e38
CarlHembrough <carlhuk@gmail.com>
2016-04-18 14:25:30 +0100

    Added existing classes from data fix tasks to be integrated into CLI functionality.

---
+++ zebedee-cli/src/main/java/com/github/onsdigital/zebedee/data/processing/CsdbFinder.java
@@ -0,0 +1,53 @@
+package com.github.onsdigital.zebedee.data.processing;
+
+import com.github.onsdigital.zebedee.model.content.item.VersionedContentItem;
+import com.github.onsdigital.zebedee.util.Log;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsdbFinder extends SimpleFileVisitor<Path> {
+
+    List<Path> files = new ArrayList<>();
+    List<String> uris = new ArrayList<>();
+    Path root;
+
+    public List<Path> find(Path root) {
+        this.root = root;
+
+        try {
+            Files.walkFileTree(root, this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return this.files;
+    }
+
+
+    @Override
+    public FileVisitResult visitFile(Path path, BasicFileAttributes attr) throws IOException {
+        // Get the uri
+        String uri = "/" + root.relativize(path).toString();
+
+        // Check json files in timeseries directories
+        if (uri.endsWith(".csdb") && uri.toString().contains("/datasets/")) {
+
+            if (VersionedContentItem.isVersionedUri(uri))
+                return FileVisitResult.CONTINUE;
+
+            //uri = uri.substring(0, uri.length() - "/data.json".length());
+
+            Log.print("Adding file with uri: %s and path %s", uri, path.toString());
+            this.files.add(path);
+            this.uris.add(uri);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}

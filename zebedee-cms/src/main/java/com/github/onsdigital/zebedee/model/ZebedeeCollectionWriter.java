ae0d881ca115524c71d20c91d340f367d38211bc
CarlHembrough <carlhuk@gmail.com>
2016-04-22 15:13:15 +0100

    Added root ContentWriter to CollectionWriter allowing data to be written to a collection that is not content.

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/model/ZebedeeCollectionWriter.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/model/ZebedeeCollectionWriter.java
@@ -74,5 +74,6 @@ public class ZebedeeCollectionWriter extends CollectionWriter {
         inProgress = new CollectionContentWriter(collection, key, collection.path.resolve(config.getInProgressFolderName()));
         complete = new CollectionContentWriter(collection, key, collection.path.resolve(config.getCompleteFolderName()));
         reviewed = new CollectionContentWriter(collection, key, collection.path.resolve(config.getReviewedFolderName()));
+        root = new CollectionContentWriter(collection, key, collection.path);
     }
 }

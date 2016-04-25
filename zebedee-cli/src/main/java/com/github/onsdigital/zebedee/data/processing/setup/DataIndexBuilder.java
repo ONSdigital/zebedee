aefeebd731353e13c91076504f65a068796793c4
CarlHembrough <carlhuk@gmail.com>
2016-04-25 13:31:43 +0100

    Added function to remove timeseries data + create new collection.

---
+++ zebedee-cli/src/main/java/com/github/onsdigital/zebedee/data/processing/setup/DataIndexBuilder.java
@@ -0,0 +1,17 @@
+package com.github.onsdigital.zebedee.data.processing.setup;
+
+import com.github.onsdigital.zebedee.data.processing.DataIndex;
+import com.github.onsdigital.zebedee.reader.ContentReader;
+
+public class DataIndexBuilder {
+    public static DataIndex buildDataIndex(ContentReader contentReader) throws InterruptedException {
+        DataIndex dataIndex = new DataIndex(contentReader);
+
+        while (!dataIndex.isIndexBuilt()) {
+            Thread.sleep(1000);
+            System.out.print(".");
+        }
+        System.out.println("");
+        return dataIndex;
+    }
+}

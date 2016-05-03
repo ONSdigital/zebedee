49b02c2bf50932fff36e4f1e531979f35c16fc37
CarlHembrough <carlhuk@gmail.com>
2016-05-03 13:09:47 +0100

    Remove collection parameter, use the content reader instead to list uris.

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/data/processing/DataPublicationFinder.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/data/processing/DataPublicationFinder.java
@@ -3,7 +3,6 @@ package com.github.onsdigital.zebedee.data.processing;
 import com.github.onsdigital.zebedee.content.page.base.Page;
 import com.github.onsdigital.zebedee.content.page.base.PageType;
 import com.github.onsdigital.zebedee.exceptions.ZebedeeException;
-import com.github.onsdigital.zebedee.model.Collection;
 import com.github.onsdigital.zebedee.reader.ContentReader;
 
 import java.io.IOException;
@@ -17,18 +16,17 @@ public class DataPublicationFinder {
      *
      * @param publishedContentReader a content reader for published content
      * @param reviewedContentReader a content reader for the collection reviewed content
-     * @param collection a collection
      *
      * @return a list of DataPublication objects
      * @throws IOException
      * @throws ZebedeeException
      */
-    public List<DataPublication> findPublications(ContentReader publishedContentReader, ContentReader reviewedContentReader, Collection collection) throws IOException, ZebedeeException {
+    public List<DataPublication> findPublications(ContentReader publishedContentReader, ContentReader reviewedContentReader) throws IOException, ZebedeeException {
 
         List<DataPublication> results = new ArrayList<>();
 
         // Loop through the uri's in the collection
-        for(String reviewedUri: collection.reviewedUris()) {
+        for (String reviewedUri : reviewedContentReader.listUris()) {
 
             // Ignoring previous versions loop through the pages
             if (!reviewedUri.toLowerCase().contains("/previous/") && reviewedUri.toLowerCase().endsWith("data.json")) {

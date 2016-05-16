c35bd0bb452e8f42fac9c6e11300028e39aad2f7
CarlHembrough <carlhuk@gmail.com>
2016-05-16 11:04:30 +0100

    Create parent directories if they do not exist

--- zebedee-cli/src/main/java/com/github/onsdigital/zebedee/data/processing/ContentMover.java
+++ zebedee-cli/src/main/java/com/github/onsdigital/zebedee/data/processing/ContentMover.java
@@ -71,6 +71,7 @@ public class ContentMover {
 
             Path destinationFilePath = destination.resolve(source.relativize(path));
             System.out.println("destinationFilePath = " + destinationFilePath);
+            Files.createDirectories(destination);
             Files.write(destinationFilePath, content.getBytes(StandardCharsets.UTF_8));
         }
     }

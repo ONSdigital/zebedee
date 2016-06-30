924fc53024fbc988e787ee23e018dd5b31f0a2d0
CarlHembrough <carlhuk@gmail.com>
2016-06-30 11:07:23 +0100

    Increase socket timeout.

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/util/Http.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/util/Http.java
@@ -1,38 +1,16 @@
 package com.github.onsdigital.zebedee.util;
 
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
-import static com.github.onsdigital.zebedee.logging.ZebedeeLogBuilder.logDebug;
-
 public class Http extends com.github.davidcarboni.httpino.Http {
-
-    public static void main(String[] args) {
-
-        HttpPost post = new HttpPost("casded");
-
-        RequestConfig requestConfig = RequestConfig.custom()
-                .setSocketTimeout(300000)
-                .setConnectTimeout(5000)
-                .setConnectionRequestTimeout(5000)
-                .build();
-
-        post.setConfig(requestConfig);
-
-        CloseableHttpClient client = HttpClients
-                .custom()
-                .setDefaultRequestConfig(requestConfig).build();
-
-        logDebug("testing").log();
-    }
 
     protected CloseableHttpClient httpClient() {
         if (httpClient == null) {
 
             RequestConfig requestConfig = RequestConfig.custom()
-                    .setSocketTimeout(5000)
+                    .setSocketTimeout(300000)
                     .setConnectTimeout(5000)
                     .setConnectionRequestTimeout(5000)
                     .build();

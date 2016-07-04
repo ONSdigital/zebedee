d8df34a40e49d4b6264c356788ae4f333b02ea58
Lloyd Griffiths <lloyd.griffiths@ons.gov.uk>
2016-07-04 09:57:05 +0100

    Add ES env vars to zebedee-reader start script

--- zebedee-reader/scripts/codedeploy/application-start.sh
+++ zebedee-reader/scripts/codedeploy/application-start.sh
@@ -3,10 +3,12 @@
 ECR_REPOSITORY_URI=
 GIT_COMMIT=
 
-docker run -d                            \
-  --env=content_dir=/content             \
-  --name=zebedee-reader                  \
-  --net=website                          \
-  --restart=always                       \
-  --volume=/var/babbage/site:/content:ro \
+docker run -d                               \
+  --env=content_dir=/content                \
+  --env=ELASTIC_SEARCH_CLUSTER=cluster      \
+  --env=ELASTIC_SEARCH_SERVER=elasticsearch \
+  --name=zebedee-reader                     \
+  --net=website                             \
+  --restart=always                          \
+  --volume=/var/babbage/site:/content:ro    \
   $ECR_REPOSITORY_URI/zebedee-reader:$GIT_COMMIT

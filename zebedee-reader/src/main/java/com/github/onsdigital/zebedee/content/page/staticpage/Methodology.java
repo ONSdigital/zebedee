4c1529e3434b83583c93b0559b9994ddb8f3423b
Crispioso <crispin.merriman@gmail.com>
2016-07-11 16:32:37 +0100

    Add PDF data tables to static methodology article content type

--- zebedee-reader/src/main/java/com/github/onsdigital/zebedee/content/page/staticpage/Methodology.java
+++ zebedee-reader/src/main/java/com/github/onsdigital/zebedee/content/page/staticpage/Methodology.java
@@ -1,11 +1,24 @@
 package com.github.onsdigital.zebedee.content.page.staticpage;
 
 import com.github.onsdigital.zebedee.content.page.base.PageType;
+import com.github.onsdigital.zebedee.content.page.statistics.dataset.DownloadSection;
+
+import java.util.List;
 
 /**
  * Created by bren on 06/07/15.
  */
 public class Methodology extends StaticArticle {
+    private List<DownloadSection> pdfTable;
+
+    public List<DownloadSection> getPdfTable() {
+        return pdfTable;
+    }
+
+    public void setPdfTable(List<DownloadSection> pdfTable) {
+        this.pdfTable = pdfTable;
+    }
+
     @Override
     public PageType getType() {
         return PageType.static_methodology;

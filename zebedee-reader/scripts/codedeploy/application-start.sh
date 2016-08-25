24943784d2e361a6c513619b5a9cf034edd33f5b
Lloyd Griffiths <lloyd.griffiths@ons.gov.uk>
2016-08-25 10:20:35 +0100

    Add splunk config to start script
    Also pulls config down from s3, decrypts and sources it before starting
    the container.

--- zebedee-reader/scripts/codedeploy/application-start.sh
+++ zebedee-reader/scripts/codedeploy/application-start.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+AWS_REGION=
+CONFIG_BUCKET=
 ECR_REPOSITORY_URI=
 GIT_COMMIT=
 
-docker run -d                               \
-  --env=content_dir=/content                \
-  --env=ELASTIC_SEARCH_CLUSTER=cluster      \
-  --env=ELASTIC_SEARCH_SERVER=elasticsearch \
-  --name=zebedee-reader                     \
-  --net=website                             \
-  --restart=always                          \
-  --volume=/var/babbage/site:/content:ro    \
+INSTANCE=$(curl -s http://instance-data/latest/meta-data/instance-id)
+CONFIG=$(aws --region $AWS_REGION ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE" "Name=key,Values=Configuration" --output text | awk '{print $5}')
+
+(aws s3 cp s3://$CONFIG_BUCKET/zebedee-reader/$CONFIG.asc . && gpg --decrypt $CONFIG.asc > $CONFIG) || exit $?
+
+source $CONFIG && docker run -d                                                         \
+  --env=content_dir=/content                                                            \
+  --env=enable_splunk_reporting=$ENABLE_SPLUNK_REPORTING                                \
+  --env=ELASTIC_SEARCH_CLUSTER=cluster                                                  \
+  --env=ELASTIC_SEARCH_SERVER=elasticsearch                                             \
+  --env=splunk_http_event_collection_host=$SPLUNK_HTTP_EVENT_COLLECTOR_HOST             \
+  --env=splunk_http_event_collection_port=$SPLUNK_HTTP_EVENT_COLLECTOR_PORT             \
+  --env=splunk_http_event_collection_uri=$SPLUNK_HTTP_EVENT_COLLECTOR_URI               \
+  --env=splunk_http_event_collection_auth_token=$SPLUNK_HTTP_EVENT_COLLECTOR_AUTH_TOKEN \
+  --name=zebedee-reader                                                                 \
+  --net=website                                                                         \
+  --restart=always                                                                      \
+  --volume=/var/babbage/site:/content:ro                                                \
   $ECR_REPOSITORY_URI/zebedee-reader:$GIT_COMMIT

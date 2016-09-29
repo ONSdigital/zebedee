6ff16e10abf681c7b7c957e5b307cfbf12459a22
Carl Hembrough <carlhuk@gmail.com>
2016-09-29 13:23:34 +0100

    Always create a new collection of notes to ensure that only one note exists.

--- zebedee-cli/src/main/java/com/github/onsdigital/zebedee/data/processing/TimeseriesUpdater.java
+++ zebedee-cli/src/main/java/com/github/onsdigital/zebedee/data/processing/TimeseriesUpdater.java
@@ -72,9 +72,7 @@ public class TimeseriesUpdater {
                 TimeSeries timeSeries = (TimeSeries) compoundContentReader.getContent(uri);
                 logDebug("Updating timeseries.").cdid(cdid).addParameter("uri", uri).log();
 
-                if (timeSeries.getNotes() == null)
-                    timeSeries.setNotes(new ArrayList<>());
-
+                timeSeries.setNotes(new ArrayList<>());
                 timeSeries.getNotes().add("Following a quality review of the Intellectual Property Products (IPP) asset in Gross Fixed Capital Formation (GFCF), analysis has shown that elements in the estimates of purchased software have been double counted from 2001 and this double counting has also uncovered a discrepancy in the modelled data prior to 2001.  Both these issues will be amended for Blue Book 17 and will have an impact on estimates of GFCF and consequently GDP and UK Economic Accounts.  The affected CDIDs are as follows:\\n\\n**IPP:** DLXP, TLPK, EQDT, EQDO\\n\\n**GFCF:** NPQX, NPQS, NPQR, NPQT\\n\\n**Business Investment:** NPEM, NPEK, NPEN, NPEL\\n\\n**GDP:** BKTL, YBHA, ABMI\\n\\n**Sectorised GFCF (In addition to above):** BKVT, DBGP, FCCJ, FCCZ, FDBM, FDCL, FDEH, IHYK, IHY6, IHYM, IHYN, IHYO, IHYP, IHYQ, IHYR, KG60, KG6I, KG6N, KG6R, KG6V, KG6W, KG6Z, KG75, KG76, KG79, KG7M, KG7N, KG7P, KG7Q, KG7S, KG7T, KH98, KH9I, KH9S, NHCJ, NHEG, NQFM, NSSU, RNZD, ROAW, RPYP, RPYQ, RPZW, RQBA, RQBB, RQBR, RQBZ, RQCM\\n\\nFurther detail can be found in [Business Investment: Quarter 2 (Apr to Jun) 2016 revised results][1]. We apologise for any inconvenience this may cause.\\n\\n\\n  [1]: http://www.ons.gov.uk/economy/grossdomesticproductgdp/bulletins/businessinvestment/previousReleases");
 
                 contentWriter.writeObject(timeSeries, uri + "/data.json");

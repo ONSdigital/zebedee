319649daa748063e4a595e68398f1543593bb062
Carl Hembrough <carlhuk@gmail.com>
2016-10-10 10:37:21 +0100

    Update startup / database scripts to source the default environment variables.

--- clear-database.sh
+++ clear-database.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-: ${db_audit_url='jdbc:postgresql://localhost:5432/audit'} # set the default connection url if its not already defined.
-: ${db_audit_username='postgres'} # set the default connection url if its not already defined.
-: ${db_audit_password='mysecretpassword'} # set the default connection url if its not already defined.
-
-export db_audit_url
-export db_audit_username
-export db_audit_password
+source ./export-default-env-vars.sh
 
 mvn -f zebedee-cms/pom.xml liquibase:dropAll
--- run-reader.sh
+++ run-reader.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source ./export-default-env-vars.sh
+
 export JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8002,server=y,suspend=n"
 export PORT="8082"
 

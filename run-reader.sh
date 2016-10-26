5f1c696121f870fd36c6a6011c16864115205e11
Dave Llewellyn <439837@gmail>
2016-10-26 11:47:08 +0100

    Removed user specific path from run-reader script

--- run-reader.sh
+++ run-reader.sh
@@ -6,8 +6,7 @@ export PORT="8082"
 # Restolino configuration
 export RESTOLINO_CLASSES="zebedee-reader/target/classes"
 export PACKAGE_PREFIX=com.github.onsdigital.zebedee.reader.api
-#export CONTENT_DIR="content"
-export CONTENT_DIR="/Users/dave/Desktop/content/zebedee_2016-01-15/master"
+export CONTENT_DIR="content"
 
 # Development: reloadable
 mvn clean package dependency:copy-dependencies -Dmaven.test.skip=true && \

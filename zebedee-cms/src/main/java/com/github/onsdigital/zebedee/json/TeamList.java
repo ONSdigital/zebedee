ed15ce35e29b6b9230ca47323b23b4ad36eac654
Ian Kent <email@iankent.co.uk>
2017-05-23 15:43:19 +0100

    remove 'members' from team list api output

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/json/TeamList.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/json/TeamList.java
@@ -1,14 +1,30 @@
 package com.github.onsdigital.zebedee.json;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by david on 21/04/2015.
  */
 public class TeamList {
-    public List<Team> teams;
+    public class TeamListTeam {
+        public int id;
+        public String name;
+
+        public TeamListTeam(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    public List<TeamListTeam> teams;
+
     public TeamList(List<Team> teamList) {
-        this.teams = teamList;
+        teams = new ArrayList<>();
+        for (Team team : teamList) {
+            teams.add(new TeamListTeam(team.id, team.name));
+        }
     }
 }
+

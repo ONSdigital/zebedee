9bf130486acb184c55601eb791eb1b697d8094bb
Carl Hembrough <carl@bitwaves.co.uk>
2017-10-09 12:01:23 +0100

    Removed redundant imports / comments.

--- zebedee-reader/src/main/java/com/github/onsdigital/zebedee/content/page/statistics/dataset/ApiDatasetLandingPage.java
+++ zebedee-reader/src/main/java/com/github/onsdigital/zebedee/content/page/statistics/dataset/ApiDatasetLandingPage.java
@@ -1,31 +1,23 @@
 package com.github.onsdigital.zebedee.content.page.statistics.dataset;
 
- import com.github.onsdigital.zebedee.content.page.base.PageType;
- import com.github.onsdigital.zebedee.content.page.statistics.base.Statistics;
- import com.github.onsdigital.zebedee.content.partial.Link;
- import com.github.onsdigital.zebedee.content.partial.markdown.MarkdownSection;
+import com.github.onsdigital.zebedee.content.page.base.PageType;
+import com.github.onsdigital.zebedee.content.page.statistics.base.Statistics;
 
- import java.util.List;
+public class ApiDatasetLandingPage extends Statistics {
 
- /**
-  * Created by
-  */
- public class ApiDatasetLandingPage extends Statistics {
+    private String apiDatasetId;
 
-     /*Body*/
-     private String apiDatasetId;
+    @Override
+    public PageType getType() {
+        return PageType.api_dataset_landing_page;
+    }
 
-     @Override
-     public PageType getType() {
-         return PageType.api_dataset_landing_page;
-     }
+    public String getapiDatasetId() {
+        return apiDatasetId;
+    }
 
-     public String getapiDatasetId() {
-         return apiDatasetId;
-     }
+    public void setapiDatasetId(String apiDatasetId) {
+        this.apiDatasetId = apiDatasetId;
+    }
 
-     public void setapiDatasetId(String apiDatasetId) {
-         this.apiDatasetId = apiDatasetId;
-     }
-
- }
+}

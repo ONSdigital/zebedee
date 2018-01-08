0b702a50898349ce4e51e64b117171db213e5198
Ian Kent <email@iankent.co.uk>
2018-01-08 10:05:58 +0000

    make auditdb configurable from environment variables
    also removes an unused config option in run.sh

--- run.sh
+++ run.sh
@@ -15,7 +15,6 @@ export audit_db_enabled=true
 mvn clean package dependency:copy-dependencies -Dmaven.test.skip=true && \
 java $JAVA_OPTS \
  -Dlogback.configurationFile=zebedee-cms/target/classes/logback.xml \
- -Dcolour_logging_enabled=$colour_logging_enabled \
  -Ddb_audit_url=$db_audit_url \
  -Daudit_db_enabled=$audit_db_enabled \
  -Ddb_audit_username=$db_audit_username \

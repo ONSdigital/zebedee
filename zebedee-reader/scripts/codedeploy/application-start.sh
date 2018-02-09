e98e25b827547a3fed4ea9e292173fd5f576e420
Lloyd Griffiths <lloyd.griffiths@ons.gov.uk>
2018-02-09 09:02:51 +0000

    hack logic to handle configuring seperate search instances for zebedee-reader
    This is a hack that won't be getting out of develop.

--- zebedee-reader/scripts/codedeploy/application-start.sh
+++ zebedee-reader/scripts/codedeploy/application-start.sh
@@ -6,15 +6,22 @@ ECR_REPOSITORY_URI=
 GIT_COMMIT=
 
 INSTANCE=$(curl -s http://instance-data/latest/meta-data/instance-id)
+INSTANCE_NUMBER=$(aws --region $AWS_REGION ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE" "Name=key,Values=Name" --output text | awk '{print $6}')
 CONFIG=$(aws --region $AWS_REGION ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE" "Name=key,Values=Configuration" --output text | awk '{print $5}')
 
 (aws s3 cp s3://$CONFIG_BUCKET/zebedee-reader/$CONFIG.asc . && gpg --decrypt $CONFIG.asc > $CONFIG) || exit $?
+
+if [[ $INSTANCE_NUMBER == 1 ]]; then
+  ELASTICSEARCH_HOST=$ELASTICSEARCH_1
+else
+  ELASTICSEARCH_HOST=$ELASTICSEARCH_2
+fi
 
 source $CONFIG && docker run -d                                                         \
   --env=content_dir=/content                                                            \
   --env=enable_splunk_reporting=$ENABLE_SPLUNK_REPORTING                                \
   --env=ELASTIC_SEARCH_CLUSTER=cluster                                                  \
-  --env=ELASTIC_SEARCH_SERVER=elasticsearch                                             \
+  --env=ELASTIC_SEARCH_SERVER=$ELASTICSEARCH_HOST                                       \
   --env=splunk_http_event_collection_host=$SPLUNK_HTTP_EVENT_COLLECTOR_HOST             \
   --env=splunk_http_event_collection_port=$SPLUNK_HTTP_EVENT_COLLECTOR_PORT             \
   --env=splunk_http_event_collection_uri=$SPLUNK_HTTP_EVENT_COLLECTOR_URI               \

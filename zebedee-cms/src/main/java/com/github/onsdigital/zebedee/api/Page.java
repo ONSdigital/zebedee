c4534929408b9745a75f059d83e56830f498e40e
Carl Hembrough <carl@bitwaves.co.uk>
2018-03-05 18:01:35 +0000

    Add HTTP delete endpoint for pages.

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/api/Page.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/api/Page.java
@@ -4,13 +4,19 @@ import com.github.davidcarboni.restolino.framework.Api;
 import com.github.onsdigital.zebedee.audit.Audit;
 import com.github.onsdigital.zebedee.configuration.Configuration;
 import com.github.onsdigital.zebedee.content.page.APIDatasetLandingPageCreationHook;
+import com.github.onsdigital.zebedee.content.page.APIDatasetLandingPageDeletionHook;
 import com.github.onsdigital.zebedee.content.page.PageTypeUpdateHook;
 import com.github.onsdigital.zebedee.content.page.PageUpdateHook;
 import com.github.onsdigital.zebedee.content.page.base.PageType;
 import com.github.onsdigital.zebedee.content.util.ContentUtil;
 import com.github.onsdigital.zebedee.exceptions.BadRequestException;
+import com.github.onsdigital.zebedee.exceptions.ConflictException;
+import com.github.onsdigital.zebedee.exceptions.NotFoundException;
+import com.github.onsdigital.zebedee.exceptions.UnauthorizedException;
 import com.github.onsdigital.zebedee.exceptions.ZebedeeException;
+import com.github.onsdigital.zebedee.model.Collection;
 import com.github.onsdigital.zebedee.persistence.CollectionEventType;
+import com.github.onsdigital.zebedee.reader.CollectionReader;
 import com.github.onsdigital.zebedee.session.model.Session;
 import com.github.onsdigital.zebedee.util.ZebedeeCmsService;
 import dp.api.dataset.DatasetAPIClient;
@@ -19,6 +25,7 @@ import org.apache.commons.io.IOUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -33,6 +40,7 @@ public class Page {
 
     private ZebedeeCmsService zebedeeCmsService;
     private PageUpdateHook pageCreationHook;
+    private PageUpdateHook pageDeletionHook;
 
     private static final URI websiteURI = URI.create(Configuration.getBabbageUrl());
 
@@ -47,6 +55,21 @@ public class Page {
                 Configuration.getDatasetAPIURL(),
                 Configuration.getDatasetAPIAuthToken());
 
+        initialisePageCreationHook(datasetAPIClient);
+        initialisePageDeletionHooks(datasetAPIClient);
+    }
+
+    private void initialisePageDeletionHooks(DatasetAPIClient datasetAPIClient) {
+        APIDatasetLandingPageDeletionHook datasetLandingPageDeletionHook =
+                new APIDatasetLandingPageDeletionHook(datasetAPIClient);
+
+        Map<PageType, PageUpdateHook> deletionHooks = new HashMap<>();
+        deletionHooks.put(PageType.api_dataset_landing_page, datasetLandingPageDeletionHook);
+
+        pageDeletionHook = new PageTypeUpdateHook(deletionHooks);
+    }
+
+    private void initialisePageCreationHook(DatasetAPIClient datasetAPIClient) {
         APIDatasetLandingPageCreationHook datasetLandingPageCreationHook =
                 new APIDatasetLandingPageCreationHook(datasetAPIClient, websiteURI);
 
@@ -58,14 +81,29 @@ public class Page {
 
     /**
      * Constructor allowing dependencies to be injected.
+     *
      * @param zebedeeCmsService
      * @param pageCreationHook
+     * @param pageDeletionHook
      */
-    public Page(ZebedeeCmsService zebedeeCmsService, PageUpdateHook pageCreationHook) {
+    public Page(ZebedeeCmsService zebedeeCmsService, PageUpdateHook pageCreationHook, PageUpdateHook pageDeletionHook) {
         this.zebedeeCmsService = zebedeeCmsService;
         this.pageCreationHook = pageCreationHook;
+        this.pageDeletionHook = pageDeletionHook;
     }
 
+    /**
+     * create a new page from the endpoint <code>/Content/[CollectionName]/?uri=[uri]</code>
+     *
+     * @param request  This should contain a X-Florence-Token header for the current session
+     * @param response Returns true or false according to whether the URI was deleted.
+     * @return
+     * @throws IOException           If an error occurs in processing data, typically to the filesystem, but also on the HTTP connection.
+     * @throws BadRequestException   If the request cannot be completed because of a problem with request parameters
+     * @throws NotFoundException     If the requested URI does not exist in the collection.
+     * @throws UnauthorizedException If the user does not have publisher permission.
+     * @throws ConflictException     If the URI is being edited in another collection
+     */
     @POST
     public void createPage(HttpServletRequest request, HttpServletResponse response) throws IOException, ZebedeeException, FileUploadException {
 
@@ -86,7 +124,8 @@ public class Page {
                 throw new BadRequestException("request is not a valid page object");
             }
 
-            pageCreationHook.onPageUpdated(page, uri);
+            if (pageCreationHook != null)
+                pageCreationHook.onPageUpdated(page, uri);
 
             try (ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes)) {
                 zebedeeCmsService.getZebedee().getCollections().createContent(
@@ -108,5 +147,46 @@ public class Page {
                     .log();
 
         }
+    }
+
+    /**
+     * Deletes page content from the endpoint <code>/Content/[CollectionName]/?uri=[uri]</code>
+     *
+     * @param request  This should contain a X-Florence-Token header for the current session
+     * @param response Returns true or false according to whether the URI was deleted.
+     * @return
+     * @throws IOException           If an error occurs in processing data, typically to the filesystem, but also on the HTTP connection.
+     * @throws BadRequestException   If the request cannot be completed because of a problem with request parameters
+     * @throws NotFoundException     If the requested URI does not exist in the collection.
+     * @throws UnauthorizedException If the user does not have publisher permission.
+     * @throws ConflictException     If the URI is being edited in another collection
+     */
+    @DELETE
+    public boolean deletePage(HttpServletRequest request, HttpServletResponse response) throws IOException,
+            ZebedeeException {
+
+        Session session = zebedeeCmsService.getSession(request);
+
+        Collection collection = zebedeeCmsService.getCollection(request);
+        String uri = request.getParameter("uri");
+
+        CollectionReader collectionReader = zebedeeCmsService.getZebedeeCollectionReader(collection, session);
+        com.github.onsdigital.zebedee.content.page.base.Page page = collectionReader.getContent(uri);
+
+        if (pageDeletionHook != null)
+            pageDeletionHook.onPageUpdated(page, uri);
+
+        boolean result = zebedeeCmsService.getZebedee().getCollections().deleteContent(collection, uri, session);
+        if (result) {
+            Audit.Event.CONTENT_DELETED
+                    .parameters()
+                    .host(request)
+                    .collection(collection)
+                    .content(uri)
+                    .user(session.getEmail())
+                    .log();
+        }
+
+        return result;
     }
 }
--- zebedee-cms/src/test/java/com/github/onsdigital/zebedee/api/PageTest.java
+++ zebedee-cms/src/test/java/com/github/onsdigital/zebedee/api/PageTest.java
@@ -6,9 +6,9 @@ import com.github.onsdigital.zebedee.content.page.statistics.dataset.ApiDatasetL
 import com.github.onsdigital.zebedee.content.util.ContentUtil;
 import com.github.onsdigital.zebedee.exceptions.ZebedeeException;
 import com.github.onsdigital.zebedee.model.Collections;
+import com.github.onsdigital.zebedee.reader.CollectionReader;
 import com.github.onsdigital.zebedee.util.ZebedeeCmsService;
 import org.apache.commons.fileupload.FileUploadException;
-import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.mock.web.DelegatingServletInputStream;
 
@@ -16,10 +16,14 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.file.Paths;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class PageTest extends ZebedeeAPIBaseTestCase {
+
+    MockPageUpdateHook pageCreationHook = new MockPageUpdateHook();
+    MockPageUpdateHook pageDeletionHook = new MockPageUpdateHook();
 
     @Test
     public void testPage_createPage() throws FileUploadException, ZebedeeException, IOException {
@@ -45,15 +49,45 @@ public class PageTest extends ZebedeeAPIBaseTestCase {
                 new DelegatingServletInputStream(
                         new ByteArrayInputStream(bodyContent.getBytes())));
 
-        MockPageUpdateHook pageUpdateHook = new MockPageUpdateHook();
-
-        Page page = new Page(zebedeeCmsService, pageUpdateHook);
+        Page page = new Page(zebedeeCmsService, pageCreationHook, pageDeletionHook);
 
         // When createPage is called
         page.createPage(mockRequest, mockResponse);
 
         // Then the page update hook is called
-        Assert.assertTrue(pageUpdateHook.wasOnPageUpdatedCalled());
+        assertTrue(pageCreationHook.wasOnPageUpdatedCalled());
+    }
+
+    @Test
+    public void testPage_deletePage() throws ZebedeeException, IOException {
+
+        // Given a page instance with mock dependencies
+        ZebedeeCmsService zebedeeCmsService = mock(ZebedeeCmsService.class);
+        Zebedee zebedee = mock(Zebedee.class);
+        Collections collections = mock(Collections.class);
+        CollectionReader collectionReader = mock(CollectionReader.class);
+        com.github.onsdigital.zebedee.model.Collection collection =
+                mock(com.github.onsdigital.zebedee.model.Collection.class);
+
+        ApiDatasetLandingPage pageToDelete = new ApiDatasetLandingPage();
+        String uri = "/";
+
+        when(mockRequest.getParameter("uri")).thenReturn(uri);
+        when(collection.getPath()).thenReturn(Paths.get(uri));
+        when(collectionReader.getContent(uri)).thenReturn(pageToDelete);
+        when(zebedeeCmsService.getZebedee()).thenReturn(zebedee);
+        when(zebedeeCmsService.getSession(mockRequest)).thenReturn(session);
+        when(zebedeeCmsService.getCollection(mockRequest)).thenReturn(collection);
+        when(zebedeeCmsService.getZebedeeCollectionReader(collection, session)).thenReturn(collectionReader);
+        when(zebedee.getCollections()).thenReturn(collections);
+
+        Page page = new Page(zebedeeCmsService, pageCreationHook, pageDeletionHook);
+
+        // When delete is called
+        page.deletePage(mockRequest, mockResponse);
+
+        // Then the page update hook is called
+        assertTrue(pageDeletionHook.wasOnPageUpdatedCalled());
     }
 
     @Override

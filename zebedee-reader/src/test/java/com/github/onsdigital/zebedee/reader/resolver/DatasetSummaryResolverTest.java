2e2a1791e8b490afe9d8ea0b2b22ab6449e0ce37
Dave Llewellyn <439837@gmail.com>
2019-03-22 07:50:44 +0000

    added unit tests for get CMD dataset summary

--- zebedee-reader/src/test/java/com/github/onsdigital/zebedee/reader/resolver/DatasetSummaryResolverTest.java
+++ zebedee-reader/src/test/java/com/github/onsdigital/zebedee/reader/resolver/DatasetSummaryResolverTest.java
@@ -3,6 +3,7 @@ package com.github.onsdigital.zebedee.reader.resolver;
 import com.github.onsdigital.zebedee.content.partial.Link;
 import com.github.onsdigital.zebedee.reader.api.bean.DatasetSummary;
 import dp.api.dataset.DatasetAPIClient;
+import dp.api.dataset.exception.DatasetAPIException;
 import dp.api.dataset.model.Dataset;
 import dp.api.dataset.model.DatasetLinks;
 import org.junit.Before;
@@ -10,10 +11,15 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.io.IOException;
 import java.net.URI;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -48,14 +54,50 @@ public class DatasetSummaryResolverTest {
         cpihDataset.setTitle("War Pigs");
         cpihDataset.setDescription(
                 "Generals gathered in their masses\n" +
-                "Just like witches at black masses\n" +
-                "Evil minds that plot destruction\n" +
-                "Sorcerers of death's construction\n" +
-                "In the fields the bodies burning\n" +
-                "As the war machine keeps turning\n" +
-                "Death and hatred to mankind\n" +
-                "Poisoning their brainwashed minds\n" +
-                "Oh lord yeah!");
+                        "Just like witches at black masses\n" +
+                        "Evil minds that plot destruction\n" +
+                        "Sorcerers of death's construction\n" +
+                        "In the fields the bodies burning\n" +
+                        "As the war machine keeps turning\n" +
+                        "Death and hatred to mankind\n" +
+                        "Poisoning their brainwashed minds\n" +
+                        "Oh lord yeah!");
+    }
+
+    @Test
+    public void testGetCMDDatasetSummary_clientDatasetAPIException() throws Exception {
+        resolver = new DatasetSummaryResolver(datasetAPIClient, true);
+
+        when(datasetAPIClient.getDataset(CPIH01_ID))
+                .thenThrow(new ClientException(""));
+
+        DatasetSummary result = resolver.getCMDDatasetSummary(PAGE_URI, datasetLink);
+        assertThat(result, is(nullValue()));
+        verify(datasetAPIClient, times(1)).getDataset(CPIH01_ID);
+    }
+
+    @Test
+    public void testGetCMDDatasetSummary_clientIOException() throws Exception {
+        resolver = new DatasetSummaryResolver(datasetAPIClient, true);
+
+        when(datasetAPIClient.getDataset(CPIH01_ID))
+                .thenThrow(new IOException(""));
+
+        DatasetSummary result = resolver.getCMDDatasetSummary(PAGE_URI, datasetLink);
+        assertThat(result, is(nullValue()));
+        verify(datasetAPIClient, times(1)).getDataset(CPIH01_ID);
+    }
+
+    @Test
+    public void testGetCMDDatasetSummary_invalidLink() throws Exception {
+        resolver = new DatasetSummaryResolver(datasetAPIClient, true);
+
+        when(datasetAPIClient.getDataset(CPIH01_ID))
+                .thenReturn(cpihDataset);
+
+        DatasetSummary result = resolver.getCMDDatasetSummary(PAGE_URI, new Link(new URI("/datasets/")));
+        assertThat(result, is(nullValue()));
+        verify(datasetAPIClient, never()).getDataset(anyString());
     }
 
     @Test
@@ -66,10 +108,10 @@ public class DatasetSummaryResolverTest {
                 .thenReturn(cpihDataset);
 
         DatasetSummary result = resolver.getCMDDatasetSummary(PAGE_URI, datasetLink);
+
         assertThat(result.getSummary(), equalTo(cpihDataset.getDescription()));
         assertThat(result.getTitle(), equalTo(cpihDataset.getTitle()));
         assertThat(result.getUri(), equalTo(cpihDataset.getLinks().getSelf().getHref()));
-
         verify(datasetAPIClient, times(1)).getDataset(CPIH01_ID);
     }
 
@@ -86,5 +128,11 @@ public class DatasetSummaryResolverTest {
         resolver = new DatasetSummaryResolver(null, false);
 
         resolver.getCMDDatasetID(PAGE_URI, new Link(new URI("/datasets/")));
+    }
+
+    static class ClientException extends DatasetAPIException {
+        public ClientException(String message) {
+            super(message);
+        }
     }
 }

de66f900c899c8687bd23614e5bc5f8a2567a174
Jon Dewi Jones <jondewijones@gmail.com>
2020-05-18 14:54:16 +0100

    Add reusable image model

---
+++ zebedee-reader/src/main/java/com/github/onsdigital/zebedee/content/partial/Image.java
@@ -0,0 +1,17 @@
+package com.github.onsdigital.zebedee.content.partial;
+
+import java.net.URI;
+
+public class Image {
+
+    private URI uri;
+
+    public URI getUri() {
+        return uri;
+    }
+
+    public void setUri(URI uri) {
+        this.uri = uri;
+    }
+    
+}

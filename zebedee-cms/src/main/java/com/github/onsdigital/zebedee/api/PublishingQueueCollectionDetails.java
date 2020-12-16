bef76644ad134e13fe9501f41351d484d569a9a2
CarlHembrough <carlhuk@gmail.com>
2020-12-16 08:21:38 +0000

    Add PublishingQueueCollectionDetails endpoint
    A clone of the existing collection details endpoint, but without populating any lists of files that are in the collection. This is due to the issue of having too many files listed in the publish queue screen, which prevents it from loading.

---
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/api/PublishingQueueCollectionDetails.java
@@ -0,0 +1,60 @@
+package com.github.onsdigital.zebedee.api;
+
+import com.github.davidcarboni.restolino.framework.Api;
+import com.github.onsdigital.zebedee.exceptions.ZebedeeException;
+import com.github.onsdigital.zebedee.json.CollectionDetail;
+import com.github.onsdigital.zebedee.session.model.Session;
+import com.github.onsdigital.zebedee.util.ZebedeeCmsService;
+import org.eclipse.jetty.http.HttpStatus;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import java.io.IOException;
+
+@Api
+public class PublishingQueueCollectionDetails {
+
+    private static ZebedeeCmsService zebedeeCmsService = ZebedeeCmsService.getInstance();
+
+    /**
+     * Retrieves a CollectionDetail object at the endpoint /PublishingQueueCollectionDetails/[CollectionName]
+     *
+     * @param request  This should contain a X-Florence-Token header for the current session
+     * @param response <ul>
+     *                 <li>If no collection exists:  {@link HttpStatus#NOT_FOUND_404}</li>
+     *                 </ul>
+     * @return the CollectionDetail.
+     * @throws IOException
+     */
+    @GET
+    public CollectionDetail get(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ZebedeeException {
+
+        com.github.onsdigital.zebedee.model.Collection collection = Collections
+                .getCollection(request);
+
+        if (collection == null) {
+            response.setStatus(HttpStatus.NOT_FOUND_404);
+            return null;
+        }
+
+        Session session = zebedeeCmsService.getSession(request);
+        if (!zebedeeCmsService.getPermissions().canView(session.getEmail(), collection.getDescription())) {
+            response.setStatus(HttpStatus.UNAUTHORIZED_401);
+            return null;
+        }
+
+        CollectionDetail result = new CollectionDetail();
+
+        result.setId(collection.getDescription().getId());
+        result.setName(collection.getDescription().getName());
+        result.setType(collection.getDescription().getType());
+        result.setPublishDate(collection.getDescription().getPublishDate());
+        result.setReleaseUri(collection.getDescription().getReleaseUri());
+
+        result.approvalStatus = collection.getDescription().approvalStatus;
+
+        return result;
+    }
+}

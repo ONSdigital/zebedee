7bb4d8280910ad08637b72e42cd8b60e07a8f4a0
Dave Llewellyn <439837@gmail.com>
2021-01-21 14:30:40 +0000

    refactor collection key store, deleted collectionKey model wrapper and refactored store impl to no longer read/write json files

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/keyring/store/CollectionKeyStoreImpl.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/keyring/store/CollectionKeyStoreImpl.java
@@ -1,17 +1,15 @@
-package com.github.onsdigital.zebedee.keyring.io;
+package com.github.onsdigital.zebedee.keyring.store;
 
-import com.github.onsdigital.zebedee.keyring.CollectionKey;
 import com.github.onsdigital.zebedee.keyring.KeyringException;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import org.apache.commons.io.IOUtils;
 
 import javax.crypto.Cipher;
 import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.RandomAccessFile;
 import java.nio.channels.Channels;
@@ -19,16 +17,16 @@ import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 /**
  * {@link java.io.File} based implementation of {@link CollectionKeyStore}. Reads and writes
- * {@link CollectionKey} objects to/from encrypted files on disk. Methods read, write & delete all aquire and exclusive
- * {@link java.nio.channels.FileLock} when accessing the collection key files.
- * <p><b>We strongly advised against using/extending this
- * code for anything other than maintaining legacy functionality in Zebedee CMS.</b></p>For all other purposes it
- * should be considered deprecated.
+ * {@link SecretKey} objects to/from encrypted files on disk. Methods read, write & delete all acquire an exclusive
+ * {@link java.nio.channels.FileLock} when accessing the key files. <p><b>We strongly advised against using/extending
+ * this code for anything other than maintaining legacy functionality in Zebedee CMS.</b></p>For all other purposes
+ * it should be considered deprecated.
  */
 public class CollectionKeyStoreImpl implements CollectionKeyStore {
 
@@ -37,13 +35,12 @@ public class CollectionKeyStoreImpl implements CollectionKeyStore {
     static final String COLLECTION_KEY_NOT_FOUND_ERR = "collectionKey not found";
     static final String KEY_DECRYPTION_ERR = "error while decrypting collectionKey file";
     static final String WRITE_KEY_ERR = "error writing collection key to store";
-    static final String COLLECTION_KEY_ID_NULL_ERR = "collectionKey.ID required but was null or empty";
-    static final String COLLECTION_KEY_KEY_NULL_ERR = "collectionKey.secretKey required but was null";
     static final String COLLECTION_KEY_ALREADY_EXISTS_ERR = "collectionKey for this collection ID already exists";
-    static final String ALGORITHM = "AES/CBC/PKCS5Padding";
+    static final String ENCRYPTION_ALGORITHM = "AES";
+    static final String CIPHER_ALGORITHM = "AES/CBC/PKCS5Padding";
+    static final String FILE_ACCESS_MODE = "rw";
 
     private Path keyringDir;
-    private Gson gson;
     private transient SecretKey masterKey;
     private transient IvParameterSpec masterIv;
 
@@ -68,21 +65,83 @@ public class CollectionKeyStoreImpl implements CollectionKeyStore {
      * use the updated encryption values.</b>
      * </p>
      *
-     * @param keyringDir the {@link Path} the read the {@link CollectionKey} files from.
-     * @param masterKey  the {@link SecretKey} to use when decrypting the {@link CollectionKey} files.
+     * @param keyringDir the {@link Path} to read the collection encryption keys files from.
+     * @param masterKey  the {@link SecretKey} to use when decrypting the collection key files.
      * @param masterIv   the {@link IvParameterSpec} to use when initializing the encryption {@link Cipher}.
      */
     public CollectionKeyStoreImpl(final Path keyringDir, final SecretKey masterKey, final IvParameterSpec masterIv) {
         this.keyringDir = keyringDir;
         this.masterKey = masterKey;
         this.masterIv = masterIv;
-        this.gson = new GsonBuilder()
-                .registerTypeAdapter(CollectionKey.class, new CollectionKeySerializer())
-                .create();
     }
 
     @Override
-    public CollectionKey read(String collectionID) throws KeyringException {
+    public SecretKey read(final String collectionID) throws KeyringException {
+        validateRead(collectionID);
+
+        byte[] keyBytes = null;
+        try (
+                RandomAccessFile raf = new RandomAccessFile(getKeyPath(collectionID), FILE_ACCESS_MODE);
+                FileChannel channel = raf.getChannel();
+                InputStream in = Channels.newInputStream(channel);
+                CipherInputStream cin = new CipherInputStream(in, getDecryptCipher());
+        ) {
+            // FileLock documentation states the lock will released when the FileChannel used to acquire it is closed.
+            // FileChannel implements autoclosable and in this case is defined inside a try-with-resources which
+            // guarantees auto closeable resourses are closed.
+            channel.lock();
+            keyBytes = IOUtils.toByteArray(cin);
+            return new SecretKeySpec(keyBytes, 0, keyBytes.length, ENCRYPTION_ALGORITHM);
+        } catch (Exception ex) {
+            throw new KeyringException(KEY_DECRYPTION_ERR, collectionID, ex);
+        } finally {
+            if (keyBytes != null) {
+                destoryKeyBytes(keyBytes);
+            }
+        }
+    }
+
+    @Override
+    public void write(final String collectionID, final SecretKey collectionKey) throws KeyringException {
+        validateWrite(collectionID, collectionKey);
+
+        try (
+                RandomAccessFile randomAccessFile = new RandomAccessFile(getKeyPath(collectionID), FILE_ACCESS_MODE);
+                FileChannel channel = randomAccessFile.getChannel();
+                OutputStream out = Channels.newOutputStream(channel);
+                CipherOutputStream cos = new CipherOutputStream(out, getEncryptCipher())
+        ) {
+            // FileLock documentation states the lock will released when the FileChannel used to acquire it is closed.
+            // FileChannel implements autoclosable and in this case is defined inside a try-with-resources which
+            // guarantees auto closeable resourses are closed.
+            channel.lock();
+            cos.write(collectionKey.getEncoded());
+            cos.flush();
+        } catch (Exception ex) {
+            throw new KeyringException(WRITE_KEY_ERR, collectionID, ex);
+        }
+    }
+
+    @Override
+    public void delete(String collectionID) throws KeyringException {
+        // TODO implementation coming soon.
+    }
+
+    private String getKeyPath(String collectionID) {
+        return keyringDir.resolve(collectionID + ".key").toString();
+    }
+
+    private Cipher getDecryptCipher() throws KeyringException {
+        try {
+            Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, masterKey, masterIv);
+            return cipher;
+        } catch (Exception ex) {
+            throw new KeyringException(ex);
+        }
+    }
+
+    private void validateRead(String collectionID) throws KeyringException {
         if (isEmpty(collectionID)) {
             throw new KeyringException(INVALID_COLLECTION_ID_ERR);
         }
@@ -92,91 +151,43 @@ public class CollectionKeyStoreImpl implements CollectionKeyStore {
         if (Files.notExists(keyPath)) {
             throw new KeyringException(COLLECTION_KEY_NOT_FOUND_ERR, collectionID);
         }
-
-        try (
-                RandomAccessFile raf = new RandomAccessFile(getKeyPath(collectionID), "rwd");
-                FileChannel channel = raf.getChannel();
-                InputStream in = Channels.newInputStream(channel);
-                CipherInputStream cin = new CipherInputStream(in, getDecryptCipher());
-                InputStreamReader reader = new InputStreamReader(cin);
-        ) {
-            // FileLock documentation states the lock will released when the FileChannel used to acquire it is closed.
-            // FileChannel implements autoclosable and in this case is defined inside a try-with-resources which
-            // guarantees auto closeable resourses are closed.
-            channel.lock();
-            return gson.fromJson(reader, CollectionKey.class);
-        } catch (Exception ex) {
-            throw new KeyringException(KEY_DECRYPTION_ERR, collectionID, ex);
-        }
     }
 
-    private String getKeyPath(String collectionID) {
-        return keyringDir.resolve(collectionID + ".json").toString();
-    }
-
-    private Cipher getDecryptCipher() throws KeyringException {
-        try {
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.DECRYPT_MODE, masterKey, masterIv);
-            return cipher;
-        } catch (Exception ex) {
-            throw new KeyringException(ex);
-        }
-    }
-
-    @Override
-    public void write(final CollectionKey key) throws KeyringException {
-        validateCollectionKey(key);
-
-        try (
-                RandomAccessFile randomAccessFile = new RandomAccessFile(getKeyPath(key.getCollectionID()), "rwd");
-                FileChannel channel = randomAccessFile.getChannel();
-                OutputStream out = Channels.newOutputStream(channel);
-                CipherOutputStream cos = new CipherOutputStream(out, getEncryptCipher())
-        ) {
-            // FileLock documentation states the lock will released when the FileChannel used to acquire it is closed.
-            // FileChannel implements autoclosable and in this case is defined inside a try-with-resources which
-            // guarantees auto closeable resourses are closed.
-            channel.lock();
-            cos.write(gson.toJson(key).getBytes());
-            cos.flush();
-        } catch (Exception ex) {
-            throw new KeyringException(WRITE_KEY_ERR, key.getCollectionID(), ex);
-        }
-    }
-
-    @Override
-    public void delete(String collectionID) throws KeyringException {
-        // TODO implementation coming soon.
-    }
-
-    private void validateCollectionKey(CollectionKey key) throws KeyringException {
-        if (key == null) {
-            throw new KeyringException(COLLECTION_KEY_NULL_ERR);
+    private void validateWrite(String collectionID, SecretKey collectionKey) throws KeyringException {
+        if (isEmpty(collectionID)) {
+            throw new KeyringException(INVALID_COLLECTION_ID_ERR);
         }
 
-        if (isEmpty(key.getCollectionID())) {
-            throw new KeyringException(COLLECTION_KEY_ID_NULL_ERR);
-        }
-
-        Path keyPath = Paths.get(getKeyPath(key.getCollectionID()));
+        Path keyPath = Paths.get(getKeyPath(collectionID));
         if (Files.exists(keyPath)) {
-            throw new KeyringException(COLLECTION_KEY_ALREADY_EXISTS_ERR, key.getCollectionID());
+            throw new KeyringException(COLLECTION_KEY_ALREADY_EXISTS_ERR, collectionID);
         }
 
-        if (key.getSecretKey() == null) {
-            throw new KeyringException(COLLECTION_KEY_KEY_NULL_ERR, key.getCollectionID());
+        if (collectionKey == null) {
+            throw new KeyringException(COLLECTION_KEY_NULL_ERR, collectionID);
         }
     }
 
     private Cipher getEncryptCipher() throws KeyringException {
         try {
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
             cipher.init(Cipher.ENCRYPT_MODE, masterKey, masterIv);
             return cipher;
         } catch (Exception ex) {
             throw new KeyringException(ex);
         }
+    }
+
+    /**
+     * Even after an object has gone out of scope it will remain on the heap until GC runs to release it. While the
+     * object remains in memory it is technically possible for someone retieve it using Java memory monitoring tools.
+     * This is very very unlikely but as an additional safeguard to minimise the window of opportunity we "zero" the
+     * SecretKey data as soon as we have finished using it ensuring its only available for the time its required.
+     *
+     * @param keyBytes the array of bytes "destroy".
+     */
+    private void destoryKeyBytes(byte[] keyBytes) {
+        Arrays.fill(keyBytes, (byte) 0);
     }
 
 }
--- zebedee-cms/src/test/java/com/github/onsdigital/zebedee/keyring/store/CollectionKeyStoreImplTest.java
+++ zebedee-cms/src/test/java/com/github/onsdigital/zebedee/keyring/store/CollectionKeyStoreImplTest.java
@@ -1,9 +1,8 @@
-package com.github.onsdigital.zebedee.keyring.io;
+package com.github.onsdigital.zebedee.keyring.store;
 
-import com.github.onsdigital.zebedee.keyring.CollectionKey;
 import com.github.onsdigital.zebedee.keyring.KeyringException;
-import com.google.gson.GsonBuilder;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,21 +13,19 @@ import javax.crypto.CipherInputStream;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.SecureRandom;
 import java.util.Base64;
 
-import static com.github.onsdigital.zebedee.keyring.io.CollectionKeyStoreImpl.COLLECTION_KEY_ALREADY_EXISTS_ERR;
-import static com.github.onsdigital.zebedee.keyring.io.CollectionKeyStoreImpl.COLLECTION_KEY_ID_NULL_ERR;
-import static com.github.onsdigital.zebedee.keyring.io.CollectionKeyStoreImpl.COLLECTION_KEY_KEY_NULL_ERR;
-import static com.github.onsdigital.zebedee.keyring.io.CollectionKeyStoreImpl.COLLECTION_KEY_NOT_FOUND_ERR;
-import static com.github.onsdigital.zebedee.keyring.io.CollectionKeyStoreImpl.COLLECTION_KEY_NULL_ERR;
-import static com.github.onsdigital.zebedee.keyring.io.CollectionKeyStoreImpl.INVALID_COLLECTION_ID_ERR;
-import static com.github.onsdigital.zebedee.keyring.io.CollectionKeyStoreImpl.KEY_DECRYPTION_ERR;
+import static com.github.onsdigital.zebedee.keyring.store.CollectionKeyStoreImpl.COLLECTION_KEY_ALREADY_EXISTS_ERR;
+import static com.github.onsdigital.zebedee.keyring.store.CollectionKeyStoreImpl.COLLECTION_KEY_NOT_FOUND_ERR;
+import static com.github.onsdigital.zebedee.keyring.store.CollectionKeyStoreImpl.COLLECTION_KEY_NULL_ERR;
+import static com.github.onsdigital.zebedee.keyring.store.CollectionKeyStoreImpl.INVALID_COLLECTION_ID_ERR;
+import static com.github.onsdigital.zebedee.keyring.store.CollectionKeyStoreImpl.KEY_DECRYPTION_ERR;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -52,10 +49,10 @@ public class CollectionKeyStoreImplTest {
     }
 
     @Test(expected = KeyringException.class)
-    public void read_shouldThrowException_ifCollectionIDNull() throws Exception {
+    public void read_collectionIDNull_shouldThrowException() throws Exception {
         store = new CollectionKeyStoreImpl(keyringDir.toPath(), null, null);
 
-        CollectionKey key = null;
+        SecretKey key = null;
         try {
             key = store.read(null);
         } catch (KeyringException ex) {
@@ -66,10 +63,10 @@ public class CollectionKeyStoreImplTest {
     }
 
     @Test(expected = KeyringException.class)
-    public void read_shouldThrowException_ifCollectionIDEmpty() throws Exception {
+    public void read_collectionIDEmpty_shouldThrowException() throws Exception {
         store = new CollectionKeyStoreImpl(keyringDir.toPath(), null, null);
 
-        CollectionKey key = null;
+        SecretKey key = null;
         try {
             key = store.read("");
         } catch (KeyringException ex) {
@@ -80,10 +77,10 @@ public class CollectionKeyStoreImplTest {
     }
 
     @Test(expected = KeyringException.class)
-    public void read_shouldThrowException_ifKeyNotFound() throws Exception {
+    public void read_keyNotFound_shouldThrowException() throws Exception {
         store = new CollectionKeyStoreImpl(keyringDir.toPath(), null, null);
 
-        CollectionKey key = null;
+        SecretKey key = null;
         try {
             key = store.read(TEST_COLLECTION_ID);
         } catch (KeyringException ex) {
@@ -95,7 +92,7 @@ public class CollectionKeyStoreImplTest {
     }
 
     @Test(expected = KeyringException.class)
-    public void testRead_shouldThrowException_ifFileInvalid() throws Exception {
+    public void testRead_keyContentInvalid_shouldThrowException() throws Exception {
         SecretKey masterKey = createNewSecretKey();
         IvParameterSpec iv = createNewInitVector();
 
@@ -104,7 +101,7 @@ public class CollectionKeyStoreImplTest {
 
         store = new CollectionKeyStoreImpl(keyringDir.toPath(), masterKey, iv);
 
-        CollectionKey actual = null;
+        SecretKey actual = null;
 
         try {
             actual = store.read(TEST_COLLECTION_ID);
@@ -117,7 +114,7 @@ public class CollectionKeyStoreImplTest {
     }
 
     @Test
-    public void testRead_Success() throws Exception {
+    public void testRead_success_shouldReturnDecryptedSecretKey() throws Exception {
         // Create the key store
         SecretKey masterKey = createNewSecretKey();
         IvParameterSpec masterIV = createNewInitVector();
@@ -125,61 +122,61 @@ public class CollectionKeyStoreImplTest {
 
         // Create a collection key to add to the keystore.
         IvParameterSpec initVector = createNewInitVector();
-        CollectionKey collectionKey = new CollectionKey(TEST_COLLECTION_ID, createNewSecretKey());
+        SecretKey collectionKey = createNewSecretKey();
 
         // Encrypt a test message using the collection key.
         String plainText = "Blackened is the end, Winter it will send, Throwing all you see Into obscurity";
-        byte[] encryptedMessage = encyrpt(plainText, collectionKey.getSecretKey(), initVector);
+        byte[] encryptedMessage = encyrpt(plainText, collectionKey, initVector);
 
         // write the key to the store.
-        store.write(collectionKey);
-        assertTrue(Files.exists(keyringDir.toPath().resolve(TEST_COLLECTION_ID + ".json")));
+        store.write(TEST_COLLECTION_ID, collectionKey);
+        assertTrue(Files.exists(keyringDir.toPath().resolve(TEST_COLLECTION_ID + ".key")));
 
         // retieve the key from the store.
-        CollectionKey actual = store.read(TEST_COLLECTION_ID);
-        assertThat(actual.getCollectionID(), equalTo(TEST_COLLECTION_ID));
+        SecretKey actual = store.read(TEST_COLLECTION_ID);
 
         // attempt to decrypt the test message using the key retrieve from the store - if its working as expected the
         // decrypted message should equal the original input.
-        byte[] decryptedBytes = decrypt(encryptedMessage, collectionKey.getSecretKey(), initVector);
+        byte[] decryptedBytes = decrypt(encryptedMessage, collectionKey, initVector);
         String decryptedMessage = new String(decryptedBytes);
         assertThat(decryptedMessage, equalTo(plainText));
     }
 
+
     @Test(expected = KeyringException.class)
-    public void testWrite_shouldThrowException_ifCollectionKeyIsNull() throws Exception {
+    public void testWrite_collectionIDNull_shouldThrowException() throws Exception {
         store = new CollectionKeyStoreImpl(keyringDir.toPath(), null, null);
 
         try {
-            store.write(null);
+            store.write(null, null);
+        } catch (KeyringException ex) {
+            assertThat(ex.getMessage(), equalTo(INVALID_COLLECTION_ID_ERR));
+            assertThat(ex.getCollectionID(), is(nullValue()));
+            throw ex;
+        }
+    }
+
+    @Test(expected = KeyringException.class)
+    public void testWrite_collectionIDEmpty_shouldThrowException() throws Exception {
+        store = new CollectionKeyStoreImpl(keyringDir.toPath(), null, null);
+
+        try {
+            store.write("", null);
+        } catch (KeyringException ex) {
+            assertThat(ex.getMessage(), equalTo(INVALID_COLLECTION_ID_ERR));
+            assertThat(ex.getCollectionID(), is(nullValue()));
+            throw ex;
+        }
+    }
+
+    @Test(expected = KeyringException.class)
+    public void testWrite_collectionKeyNull_shouldThrowException() throws Exception {
+        store = new CollectionKeyStoreImpl(keyringDir.toPath(), null, null);
+
+        try {
+            store.write(TEST_COLLECTION_ID, null);
         } catch (KeyringException ex) {
             assertThat(ex.getMessage(), equalTo(COLLECTION_KEY_NULL_ERR));
-            assertThat(ex.getCollectionID(), is(nullValue()));
-            throw ex;
-        }
-    }
-
-    @Test(expected = KeyringException.class)
-    public void testWrite_shouldThrowException_ifCollectionKeyIDIsNull() throws Exception {
-        store = new CollectionKeyStoreImpl(keyringDir.toPath(), null, null);
-
-        try {
-            store.write(new CollectionKey(null, null));
-        } catch (KeyringException ex) {
-            assertThat(ex.getMessage(), equalTo(COLLECTION_KEY_ID_NULL_ERR));
-            assertThat(ex.getCollectionID(), is(nullValue()));
-            throw ex;
-        }
-    }
-
-    @Test(expected = KeyringException.class)
-    public void testWrite_shouldThrowException_ifCollectionKeySecretKeyIsNull() throws Exception {
-        store = new CollectionKeyStoreImpl(keyringDir.toPath(), null, null);
-
-        try {
-            store.write(new CollectionKey(TEST_COLLECTION_ID, null));
-        } catch (KeyringException ex) {
-            assertThat(ex.getMessage(), equalTo(COLLECTION_KEY_KEY_NULL_ERR));
             assertThat(ex.getCollectionID(), equalTo(TEST_COLLECTION_ID));
             throw ex;
         }
@@ -196,7 +193,7 @@ public class CollectionKeyStoreImplTest {
 
         try {
             // attempt to write another key with the same collection ID.
-            store.write(new CollectionKey(TEST_COLLECTION_ID, createNewSecretKey()));
+            store.write(TEST_COLLECTION_ID, createNewSecretKey());
         } catch (KeyringException ex) {
             assertThat(ex.getMessage(), equalTo(COLLECTION_KEY_ALREADY_EXISTS_ERR));
             assertThat(ex.getCollectionID(), equalTo(TEST_COLLECTION_ID));
@@ -212,32 +209,29 @@ public class CollectionKeyStoreImplTest {
         store = new CollectionKeyStoreImpl(keyringDir.toPath(), masterKey, masterIV);
 
         // Create a new collection key and write it's to the target destination.
-        CollectionKey input = new CollectionKey(TEST_COLLECTION_ID, createNewSecretKey());
-        store.write(input);
+        SecretKey collectionKey = createNewSecretKey();
+        store.write(TEST_COLLECTION_ID, collectionKey);
 
         // check the file exists
-        File f = keyringDir.toPath().resolve(TEST_COLLECTION_ID + ".json").toFile();
+        File f = keyringDir.toPath().resolve(TEST_COLLECTION_ID + ".key").toFile();
         assertTrue(Files.exists(f.toPath()));
 
         // Attempt to decrypt the file - if successful we can assume the encryption was also successful.
         Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE, masterKey, masterIV);
 
-        CollectionKey result = null;
+        SecretKey actualKey = null;
         try (
                 FileInputStream fin = new FileInputStream(f);
                 CipherInputStream cin = new CipherInputStream(fin, cipher);
-                InputStreamReader reader = new InputStreamReader(cin)
         ) {
-            result = new GsonBuilder()
-                    .registerTypeAdapter(CollectionKey.class, new CollectionKeySerializer())
-                    .create()
-                    .fromJson(reader, CollectionKey.class);
+            byte[] keyBytes = IOUtils.toByteArray(cin);
+            actualKey = new SecretKeySpec(keyBytes, 0, keyBytes.length, "AES");
         }
 
         // Verify the Collection key we created from decrypting the file and marshalling into the target object type
         // matches the origin input object.
-        assertThat(result, equalTo(input));
+        assertThat(actualKey, equalTo(collectionKey));
     }
 
     SecretKey createNewSecretKey() throws Exception {
@@ -268,17 +262,8 @@ public class CollectionKeyStoreImplTest {
         return cipher.doFinal(bytes);
     }
 
-    CollectionKey createPlainTextCollectionKeyFile(String collectionID, SecretKey secretKey) throws Exception {
-        CollectionKey key = new CollectionKey(collectionID, secretKey);
-        byte[] json = new GsonBuilder()
-                .registerTypeAdapter(CollectionKey.class, new CollectionKeySerializer())
-                .setPrettyPrinting()
-                .create().toJson(key).getBytes();
-
-        Path p = keyringDir.toPath().resolve(collectionID + ".json");
-
-        FileUtils.writeByteArrayToFile(p.toFile(), json);
-
-        return key;
+    void createPlainTextCollectionKeyFile(String collectionID, SecretKey secretKey) throws Exception {
+        Path p = keyringDir.toPath().resolve(collectionID + ".key");
+        FileUtils.writeByteArrayToFile(p.toFile(), "This is not a valid secret key".getBytes());
     }
 }

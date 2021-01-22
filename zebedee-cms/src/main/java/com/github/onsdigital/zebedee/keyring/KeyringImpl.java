857dd290ef79f463d0fe674e34248e00e8e9566f
Dave Llewellyn <439837@gmail.com>
2021-01-22 11:58:42 +0000

    fixing code to work with the latest updates from feature/master-keyring

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/keyring/KeyringImpl.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/keyring/KeyringImpl.java
@@ -1,86 +1,65 @@
 package com.github.onsdigital.zebedee.keyring;
 
-import com.github.onsdigital.zebedee.keyring.io.CollectionKeyReadWriter;
+import com.github.onsdigital.zebedee.keyring.store.CollectionKeyStore;
+import liquibase.util.StringUtils;
 
 import javax.crypto.SecretKey;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.locks.ReentrantLock;
-
-import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 public class KeyringImpl implements Keyring {
 
     static final String INVALID_COLLECTION_ID_ERR_MSG = "expected collection ID but was null or empty";
     static final String INVALID_SECRET_KEY_ERR_MSG = "expected secret key but was null";
+    static final String DUPLICATE_KEY_ERR_MSG = "collection key already exists for this collection";
     static final String KEY_MISMATCH_ERR_MSG =
-            "add unsuccessful a SecretKey with a different value is already associated with this collection ID";
+            "add unsuccessful a different SecretKey already exists for a collection with ID";
     static final String KEY_NOT_FOUND_ERR_MSG = "collectionKey not found for this collection ID";
 
-    private CollectionKeyReadWriter collectionKeyReadWriter;
-    private Map<String, CollectionKey> cache;
+    private CollectionKeyStore keyStore;
+    private Map<String, SecretKey> cache;
+    private transient Object lock = new Object();
 
-    // Lock is used to ensure thread safety of the cache map. Keyring useage is fairly low so the performance impact
-    // should be reasonably small.
-    private final ReentrantLock lock = new ReentrantLock();
-
-    public KeyringImpl(final CollectionKeyReadWriter collectionKeyReadWriter) {
-        this(collectionKeyReadWriter, new ConcurrentHashMap<>());
+    public KeyringImpl(final CollectionKeyStore keyStore) {
+        this(keyStore, new HashMap<>());
     }
 
-    KeyringImpl(final CollectionKeyReadWriter collectionKeyReadWriter, final ConcurrentHashMap<String, CollectionKey> cache) {
-        this.collectionKeyReadWriter = collectionKeyReadWriter;
+    KeyringImpl(final CollectionKeyStore keyStore, final Map<String, SecretKey> cache) {
+        this.keyStore = keyStore;
         this.cache = cache;
     }
 
     @Override
     public void add(final String collectionID, final SecretKey secretKey) throws KeyringException {
-        lock.lock();
+        validateAddKeyParams(collectionID, secretKey);
 
-        try {
-            validateAddNewKeyParams(collectionID, secretKey);
-
-            if (canAddKey(collectionID, secretKey)) {
-                CollectionKey collectionKey = new CollectionKey(collectionID, secretKey);
-
-                collectionKeyReadWriter.write(collectionKey);
-                cache.put(collectionID, collectionKey);
+        synchronized (lock) {
+            if (!isCached(collectionID, secretKey)) {
+                keyStore.write(collectionID, secretKey);
+                cache.put(collectionID, secretKey);
             }
-        } finally {
-            lock.unlock();
         }
+
     }
 
-    private void validateAddNewKeyParams(String collectionID, SecretKey secretKey) throws KeyringException {
-        if (isEmpty(collectionID)) {
+    private void validateAddKeyParams(String collectionID, SecretKey secretKey) throws KeyringException {
+        if (StringUtils.isEmpty(collectionID)) {
             throw new KeyringException(INVALID_COLLECTION_ID_ERR_MSG);
         }
 
         if (secretKey == null) {
-            throw new KeyringException(INVALID_SECRET_KEY_ERR_MSG);
+            throw new KeyringException(INVALID_SECRET_KEY_ERR_MSG, collectionID);
         }
     }
 
-    /**
-     * Check if the a new {@link CollectionKey} for this collectionID can be added.
-     *
-     * @param collectionID
-     * @param secretKey
-     * @return True if no entry exists for this collectionID. False if there is an existing entry for the collection
-     * ID and the {@link SecretKey} values match.
-     * @throws KeyringException thrown if there is an existing entry for the collection ID but <b>the {@link SecretKey}
-     *                          values do not match</b>. In this case we don't want to overrite the existing encryption
-     *                          key value so bomb out with an error as something has gone wrong in the collection
-     *                          creation process.
-     */
-    private boolean canAddKey(String collectionID, SecretKey secretKey) throws KeyringException {
+    private boolean isCached(String collectionID, SecretKey keyToAdd) throws KeyringException {
         if (!cache.containsKey(collectionID)) {
-            return true;
+            return false;
         }
 
-        CollectionKey existingEntry = cache.get(collectionID);
-        if (secretKey.equals(existingEntry.getSecretKey())) {
-            return false;
+        SecretKey cachedKey = cache.get(collectionID);
+        if (keyToAdd.equals(cachedKey)) {
+            return true;
         }
 
         throw new KeyringException(KEY_MISMATCH_ERR_MSG, collectionID);
@@ -88,33 +67,7 @@ public class KeyringImpl implements Keyring {
 
     @Override
     public SecretKey get(String collectionID) throws KeyringException {
-        if (isEmpty(collectionID)) {
-            throw new KeyringException(INVALID_COLLECTION_ID_ERR_MSG);
-        }
-
-        lock.lock();
-        try {
-            CollectionKey collectionKey = cache.get(collectionID);
-            if (collectionKey != null) {
-                return getSecretKey(collectionKey);
-            }
-
-            collectionKey = collectionKeyReadWriter.read(collectionID);
-            if (collectionKey != null) {
-                return getSecretKey(collectionKey);
-            }
-
-            throw new KeyringException(KEY_NOT_FOUND_ERR_MSG);
-        } finally {
-            lock.unlock();
-        }
-    }
-
-    private SecretKey getSecretKey(CollectionKey collectionKey) throws KeyringException {
-        if (collectionKey.getSecretKey() == null) {
-            throw new KeyringException(INVALID_SECRET_KEY_ERR_MSG);
-        }
-        return collectionKey.getSecretKey();
+        return null;
     }
 
     @Override
--- zebedee-cms/src/test/java/com/github/onsdigital/zebedee/keyring/KeyringImplTest.java
+++ zebedee-cms/src/test/java/com/github/onsdigital/zebedee/keyring/KeyringImplTest.java
@@ -1,63 +1,46 @@
 package com.github.onsdigital.zebedee.keyring;
 
-import com.github.onsdigital.zebedee.keyring.io.CollectionKeyReadWriter;
+import com.github.onsdigital.zebedee.keyring.store.CollectionKeyStore;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import static com.github.onsdigital.zebedee.keyring.KeyringImpl.INVALID_COLLECTION_ID_ERR_MSG;
 import static com.github.onsdigital.zebedee.keyring.KeyringImpl.INVALID_SECRET_KEY_ERR_MSG;
 import static com.github.onsdigital.zebedee.keyring.KeyringImpl.KEY_MISMATCH_ERR_MSG;
-import static com.github.onsdigital.zebedee.keyring.KeyringImpl.KEY_NOT_FOUND_ERR_MSG;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verifyZeroInteractions;
 
 public class KeyringImplTest {
 
     private static final String TEST_COLLECTION_ID = "138"; // We are 138! We are 138 \m/
 
     private Keyring keyring;
-    private ConcurrentHashMap<String, CollectionKey> cache;
+    private Map<String, SecretKey> cache;
 
     @Mock
-    private CollectionKeyReadWriter collectionKeyReadWriter;
+    private CollectionKeyStore keyStore;
 
     @Mock
     private SecretKey secretKey;
-
-    private CollectionKey collectionKey;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
-        this.cache = new ConcurrentHashMap<>();
-        this.collectionKey = new CollectionKey(TEST_COLLECTION_ID, secretKey);
-        this.keyring = new KeyringImpl(collectionKeyReadWriter, cache);
+        this.cache = new HashMap<>();
+        this.keyring = new KeyringImpl(keyStore, cache);
     }
 
     @Test(expected = KeyringException.class)
@@ -88,185 +71,64 @@ public class KeyringImplTest {
             keyring.add(TEST_COLLECTION_ID, null);
         } catch (KeyringException ex) {
             assertThat(ex.getMessage(), equalTo(INVALID_SECRET_KEY_ERR_MSG));
-            assertTrue(cache.isEmpty());
-            throw ex;
-        }
-    }
-
-    @Test(expected = KeyringException.class)
-    public void testAdd_collectionKeyWriterError_shouldThrowException() throws Exception {
-        doThrow(KeyringException.class)
-                .when(collectionKeyReadWriter)
-                .write(collectionKey);
-
-        try {
-            keyring.add(TEST_COLLECTION_ID, secretKey);
-        } catch (KeyringException ex) {
-            verify(collectionKeyReadWriter, times(1)).write(collectionKey);
-
+            assertThat(ex.getCollectionID(), equalTo(TEST_COLLECTION_ID));
             assertTrue(cache.isEmpty());
             throw ex;
         }
     }
 
     @Test
-    public void testAdd_duplicateEntry_shouldDoNothing() throws Exception {
-        cache.put(collectionKey.getCollectionID(), collectionKey);
+    public void testAdd_keyExistsInCache_shouldDoNothing() throws Exception {
+        cache.put(TEST_COLLECTION_ID, secretKey);
         assertThat(cache.size(), equalTo(1));
 
-        keyring.add(collectionKey.getCollectionID(), collectionKey.getSecretKey());
+        keyring.add(TEST_COLLECTION_ID, secretKey);
 
         assertThat(cache.size(), equalTo(1));
-        verify(collectionKeyReadWriter, never()).write(collectionKey);
+        assertTrue(cache.containsKey(TEST_COLLECTION_ID));
+        assertThat(cache.get(TEST_COLLECTION_ID), equalTo(secretKey));
+        verifyZeroInteractions(keyStore);
     }
 
     @Test(expected = KeyringException.class)
-    public void testAdd_duplicateEntryDifferentKeyValue_shouldThrowException() throws Exception {
-        cache.put(collectionKey.getCollectionID(), collectionKey);
-        assertThat(cache.size(), equalTo(1));
+    public void testAdd_keyWithDiffentValueExistsInCache_shouldThrowException() throws Exception {
+        SecretKey key2 = mock(SecretKey.class);
+        cache.put(TEST_COLLECTION_ID, key2);
 
         try {
-            keyring.add(collectionKey.getCollectionID(), mock(SecretKey.class));
+            keyring.add(TEST_COLLECTION_ID, secretKey);
         } catch (KeyringException ex) {
+            verifyZeroInteractions(keyStore);
             assertThat(cache.size(), equalTo(1));
-            assertThat(cache.get(TEST_COLLECTION_ID).getSecretKey(), equalTo(secretKey));
-
-            verify(collectionKeyReadWriter, never()).write(collectionKey);
             assertThat(ex.getMessage(), equalTo(KEY_MISMATCH_ERR_MSG));
             assertThat(ex.getCollectionID(), equalTo(TEST_COLLECTION_ID));
             throw ex;
         }
     }
 
+    @Test(expected = KeyringException.class)
+    public void testAdd_keystoreThrowsException_shouldThrowException() throws Exception {
+        doThrow(KeyringException.class)
+                .when(keyStore)
+                .write(TEST_COLLECTION_ID, secretKey);
+        try {
+            keyring.add(TEST_COLLECTION_ID, secretKey);
+        } catch (KeyringException ex) {
+            verify(keyStore, times(1)).write(TEST_COLLECTION_ID, secretKey);
+            assertTrue(cache.isEmpty());
+            throw ex;
+        }
+    }
+
     @Test
-    public void testAdd_success_shouldCallReadWriterAndPutKeyInCache() throws Exception {
+    public void testAdd_success_shouldWriteKeyToStoreAndCache() throws Exception {
         keyring.add(TEST_COLLECTION_ID, secretKey);
 
-        assertFalse(cache.isEmpty());
+        verify(keyStore, times(1)).write(TEST_COLLECTION_ID, secretKey);
         assertThat(cache.size(), equalTo(1));
-        assertThat(cache.get(TEST_COLLECTION_ID), equalTo(collectionKey));
-
-        verify(collectionKeyReadWriter, times(1)).write(collectionKey);
+        assertTrue(cache.containsKey(TEST_COLLECTION_ID));
+        assertThat(cache.get(TEST_COLLECTION_ID), equalTo(secretKey));
     }
 
-
-    /**
-     * Test Add with x concurrent requests. Verifies that the the cache contains the correct/expected values when
-     * being access by several threads at the same time.
-     *
-     * @throws Exception test failed...obviously
-     */
-    @Test
-    public void testAdd_concurrent_shouldAddAllKeys() throws Exception {
-        KeyGenerator gen = KeyGenerator.getInstance("AES");
-
-        int concurrentAddsCount = 50;
-
-        Map<String, CollectionKey> keysAddedConcurrently = new HashMap<>();
-
-        // create a list of concurrent add key tasks and keep and note of the collectionID -> Key mappings.
-        List<Callable<String>> tasks = new ArrayList<>();
-        for (int id = 0; id < concurrentAddsCount; id++) {
-            SecretKey key = gen.generateKey();
-            String collectionID = String.valueOf(id);
-
-            keysAddedConcurrently.put(collectionID, new CollectionKey(collectionID, key));
-
-            tasks.add(() -> {
-                keyring.add(collectionID, key);
-                return collectionID;
-            });
-        }
-
-        // create a thread executor and submit all the add key tasks concurrently.
-        ExecutorService executor = Executors.newFixedThreadPool(concurrentAddsCount);
-        List<Future<String>> results = executor.invokeAll(tasks);
-
-        assertFalse(cache.isEmpty());
-        assertThat(cache.size(), equalTo(concurrentAddsCount));
-        verify(collectionKeyReadWriter, times(concurrentAddsCount)).write(any(CollectionKey.class));
-
-        for (Map.Entry<String, CollectionKey> entry : keysAddedConcurrently.entrySet()) {
-            String id = entry.getKey();
-
-            CollectionKey expected = keysAddedConcurrently.get(id);
-            CollectionKey actual = cache.get(id);
-
-            verify(collectionKeyReadWriter, times(1)).write(expected);
-            assertThat(actual, is(notNullValue()));
-            assertThat(actual, equalTo(expected));
-        }
-    }
-
-    @Test(expected = KeyringException.class)
-    public void testGet_collectionIDNull_shouldThrowException() throws Exception {
-        try {
-            keyring.get(null);
-        } catch (KeyringException ex) {
-            assertThat(ex.getMessage(), equalTo(INVALID_COLLECTION_ID_ERR_MSG));
-            throw ex;
-        }
-    }
-
-    @Test(expected = KeyringException.class)
-    public void testGet_collectionIDEmpty_shouldThrowException() throws Exception {
-        try {
-            keyring.get("");
-        } catch (KeyringException ex) {
-            assertThat(ex.getMessage(), equalTo(INVALID_COLLECTION_ID_ERR_MSG));
-            throw ex;
-        }
-    }
-
-    @Test(expected = KeyringException.class)
-    public void testGet_keyNotExist_shouldThrowException() throws Exception {
-        when(collectionKeyReadWriter.read(TEST_COLLECTION_ID))
-                .thenReturn(null);
-
-        try {
-            keyring.get(TEST_COLLECTION_ID);
-        } catch (KeyringException ex) {
-            verify(collectionKeyReadWriter, times(1)).read(TEST_COLLECTION_ID);
-
-            assertThat(ex.getMessage(), equalTo(KEY_NOT_FOUND_ERR_MSG));
-            throw ex;
-        }
-    }
-
-    @Test(expected = KeyringException.class)
-    public void testGet_secretKeyValueNull_shouldThrowException() throws Exception {
-        when(collectionKeyReadWriter.read(TEST_COLLECTION_ID))
-                .thenReturn(new CollectionKey(TEST_COLLECTION_ID, null));
-
-        try {
-            keyring.get(TEST_COLLECTION_ID);
-        } catch (KeyringException ex) {
-            verify(collectionKeyReadWriter, times(1)).read(TEST_COLLECTION_ID);
-
-            assertThat(ex.getMessage(), equalTo(INVALID_SECRET_KEY_ERR_MSG));
-            throw ex;
-        }
-    }
-
-    @Test
-    public void testGet_keyNotExistInCache_shouldReadKeyFromDisk() throws Exception {
-        when(collectionKeyReadWriter.read(TEST_COLLECTION_ID))
-                .thenReturn(collectionKey);
-
-        SecretKey actual = keyring.get(TEST_COLLECTION_ID);
-
-        assertThat(actual, equalTo(secretKey));
-        verify(collectionKeyReadWriter, times(1)).read(TEST_COLLECTION_ID);
-    }
-
-    @Test
-    public void testGet_keyExistInCache_shouldReturnCachedValue() throws Exception {
-        cache.put(TEST_COLLECTION_ID, collectionKey);
-
-        SecretKey actual = keyring.get(TEST_COLLECTION_ID);
-
-        assertThat(actual, equalTo(secretKey));
-        verify(collectionKeyReadWriter, never()).read(TEST_COLLECTION_ID);
-    }
 
 }

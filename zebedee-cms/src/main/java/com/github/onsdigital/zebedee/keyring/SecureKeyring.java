11773c83d0a9fffd43e8076a90f0cfc0f9f4f82a
Dave Llewellyn <439837@gmail.com>
2021-02-09 16:03:18 +0000

    addressed PR comment

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/keyring/SecureKeyring.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/keyring/SecureKeyring.java
@@ -7,7 +7,7 @@ public interface SecureKeyring {
     /**
      * Populate the Keyring from an unlocked {@link User#keyring()}
      *
-     * @param user the user to use to populate the keyring.
+     * @param user the user that populates the keyring.
      * @throws KeyringException problem populating the keyring.
      */
     void populateFromUser(User user) throws KeyringException;

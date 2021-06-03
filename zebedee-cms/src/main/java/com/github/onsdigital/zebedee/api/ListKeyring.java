b6ec6f0eaeec28b7b14511cef72fd971c005d493
Dave Llewellyn <439837@gmail.com>
2021-06-03 09:38:33 +0100

    Added new API to list collection IDs of keys stored in users keyring

---
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/api/ListKeyring.java
@@ -0,0 +1,120 @@
+package com.github.onsdigital.zebedee.api;
+
+import com.github.davidcarboni.restolino.framework.Api;
+import com.github.onsdigital.zebedee.exceptions.BadRequestException;
+import com.github.onsdigital.zebedee.exceptions.InternalServerError;
+import com.github.onsdigital.zebedee.exceptions.NotFoundException;
+import com.github.onsdigital.zebedee.exceptions.UnauthorizedException;
+import com.github.onsdigital.zebedee.exceptions.ZebedeeException;
+import com.github.onsdigital.zebedee.keyring.Keyring;
+import com.github.onsdigital.zebedee.keyring.KeyringException;
+import com.github.onsdigital.zebedee.permissions.service.PermissionsService;
+import com.github.onsdigital.zebedee.session.model.Session;
+import com.github.onsdigital.zebedee.session.service.Sessions;
+import com.github.onsdigital.zebedee.user.model.User;
+import com.github.onsdigital.zebedee.user.service.UsersService;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import java.io.IOException;
+import java.util.Set;
+
+import static com.github.onsdigital.zebedee.logging.CMSLogEvent.error;
+
+@Api
+public class ListKeyring {
+
+    private Keyring keyring;
+    private Sessions sessions;
+    private PermissionsService permissionsService;
+    private UsersService usersService;
+
+    public ListKeyring() {
+        this(Root.zebedee.getCollectionKeyring(),
+                Root.zebedee.getSessions(),
+                Root.zebedee.getPermissionsService(),
+                Root.zebedee.getUsersService());
+    }
+
+    public ListKeyring(final Keyring keyring, final Sessions sessions,
+                       final PermissionsService permissionsService, final UsersService usersService) {
+        this.keyring = keyring;
+        this.sessions = sessions;
+        this.permissionsService = permissionsService;
+        this.usersService = usersService;
+    }
+
+    @GET
+    public Set<String> listUserKeys(HttpServletRequest request, HttpServletResponse response) throws ZebedeeException {
+        checkPermission(getSession(request));
+        User user = getUser(getEmail(request));
+        return listKeyring(user);
+    }
+
+    Session getSession(HttpServletRequest request) throws UnauthorizedException, InternalServerError {
+        Session session;
+        try {
+            session = sessions.get(request);
+        } catch (IOException ex) {
+            error().exception(ex).log("error getting session from request");
+            throw new InternalServerError("internal server error");
+        }
+
+        if (session == null) {
+            throw new UnauthorizedException("user not authorised to access this resource");
+        }
+
+        return session;
+    }
+
+    void checkPermission(Session session) throws UnauthorizedException, InternalServerError {
+        boolean isAdmin;
+        try {
+            isAdmin = permissionsService.isAdministrator(session);
+        } catch (IOException ex) {
+            error().exception(ex).log("error checking user permissions");
+            throw new InternalServerError("internal server error");
+        }
+
+        if (!isAdmin) {
+            throw new UnauthorizedException("admin permissions required to access this resource");
+        }
+    }
+
+    String getEmail(HttpServletRequest request) throws BadRequestException {
+        String email = request.getParameter("email");
+
+        if (StringUtils.isEmpty(email)) {
+            throw new BadRequestException("user email required but was null/empty");
+        }
+
+        return email;
+    }
+
+    User getUser(String email) throws InternalServerError, NotFoundException, BadRequestException {
+        User user;
+        try {
+            user = usersService.getUserByEmail(email);
+        } catch (IOException ex) {
+            error().user(email).exception(ex).log("error getting user by email");
+            throw new InternalServerError("internal server error");
+        }
+
+        if (user == null) {
+            throw new NotFoundException("requested user was not found");
+        }
+
+        return user;
+    }
+
+    Set<String> listKeyring(User user) throws InternalServerError {
+        try {
+            return keyring.list(user);
+        } catch (KeyringException ex) {
+            error().user(user.getEmail()).exception(ex).log("error listing user keyring");
+            throw new InternalServerError("internal server error");
+        }
+    }
+}

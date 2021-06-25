3248fa99d0344feaa637eb80f77591bbf60f9051
AWitcherONS <ann.witcher@ons.gov.uk>
2021-06-25 15:01:36 +0100

    Resolve final PR Comment

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/session/store/SessionsThreadLocalImpl.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/session/store/SessionsThreadLocalImpl.java
@@ -2,11 +2,11 @@ package com.github.onsdigital.zebedee.session.store;
 
 import com.github.onsdigital.zebedee.session.store.exceptions.*;
 import org.apache.commons.lang.StringUtils;
+import com.github.onsdigital.JWTHandlerImpl;
 import com.github.onsdigital.exceptions.*;
 import com.github.onsdigital.impl.*;
 import com.github.onsdigital.interfaces.*;
 import javax.servlet.http.HttpServletRequest;
-import com.github.onsdigital.JWTHandlerImpl;
 
 /**
  * This method creates a ThreadLocal for the validated jwt  
@@ -14,15 +14,17 @@ import com.github.onsdigital.JWTHandlerImpl;
  * as part of implementing dp-identity-api */
 
 public class SessionsThreadLocalImpl implements SessionsThreadLocal {
-
-    private static HttpServletRequest request;
-    private static String secretKey;
     
     private static ThreadLocal<UserDataPayload> store =
             new ThreadLocal<>();
 
     private JWTHandler jwtHandler = new JWTHandlerImpl();
+    public SessionsThreadLocalImpl(JWTHandler jwtHandler) {
+        this.jwtHandler = jwtHandler;
+    }
 
+    private static HttpServletRequest request;
+    private static String secretKey;
     public SessionsThreadLocalImpl(HttpServletRequest request, String secretKey) {
         this.request = request;
         this.secretKey = secretKey;
@@ -49,7 +51,7 @@ public class SessionsThreadLocalImpl implements SessionsThreadLocal {
         /** From the two arguments adds the result of the jwtvalidation to Threadlocal store   */
         try {
             store.set(
-            jwtHandler.verifyJWT(request.getHeader("Authorization"), secretKey));
+                jwtHandler.verifyJWT(request.getHeader("Authorization"), secretKey));
         } catch (JWTTokenExpiredException e) {
             throw new SessionsTokenExpiredException("JWT verification failed as token is expired.");
         } catch (JWTVerificationException e) {

0b87cdf37ff2c2de9ba029bc5c0d82d5ccae7afd
janderson2 <j.anderson.ons@gmail.com>
2021-12-08 17:29:29 +0000

    Remove unnecessary import

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/api/Root.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/api/Root.java
@@ -35,7 +35,6 @@ import java.util.Map;
 
 import static com.github.onsdigital.logging.v2.event.SimpleEvent.error;
 import static com.github.onsdigital.logging.v2.event.SimpleEvent.info;
-import static com.github.onsdigital.zebedee.configuration.CMSFeatureFlags.cmsFeatureFlags;
 
 public class Root {
 

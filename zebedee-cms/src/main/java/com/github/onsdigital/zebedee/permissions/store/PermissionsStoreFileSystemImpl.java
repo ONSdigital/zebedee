b6b053b4486a3156420f4b7de50617b37c35df78
janderson2 <j.anderson.ons@gmail.com>
2022-02-04 13:03:44 +0000

    Remove unused data vis from access mapping
    Remove the now unused data vis list from the access mapping and remove
    the migration logic that was used to merge the data vis and publishing
    team lists as the migration has long since completed in production.

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/permissions/store/PermissionsStoreFileSystemImpl.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/permissions/store/PermissionsStoreFileSystemImpl.java
@@ -23,7 +23,6 @@ import static com.github.onsdigital.logging.v2.event.SimpleEvent.info;
 public class PermissionsStoreFileSystemImpl implements PermissionsStore {
 
     static final String PERMISSIONS_FILE = "accessMapping.json";
-    static final String DATA_VIS_KEY = "dataVisualisationPublishers";
     static final String PUBLISHERS_KEY = "digitalPublishingTeam";
     static final String ADMINS_KEY = "administrators";
     static final String COLLECTIONS_KEY = "collections";
@@ -48,44 +47,6 @@ public class PermissionsStoreFileSystemImpl implements PermissionsStore {
             jsonPath.toFile().createNewFile();
             try (OutputStream output = Files.newOutputStream(jsonPath)) {
                 Serialiser.serialise(output, new AccessMapping());
-            }
-        } else {
-            migrateDataVisUsers(jsonPath);
-        }
-    }
-
-    /**
-     * Temporary legacy migration. Transfer deprecated Data Visualisation Publisher users to Digital Publishing team.
-     * Can be reomved once all users have been safely migrated.
-     */
-    private static void migrateDataVisUsers(Path p) throws IOException {
-        Map<String, Object> accessMapping = Serialiser.deserialise(p, Map.class);
-        if (accessMapping.containsKey(DATA_VIS_KEY)) {
-
-            List<String> dataVisualisationPublishers = (List<String>) accessMapping.get(DATA_VIS_KEY);
-            if (dataVisualisationPublishers != null && !dataVisualisationPublishers.isEmpty()) {
-
-                info().log("Migrating users from Data Visualisation team to Digital Publishing team.");
-
-                List<String> digitalPublishingTeam = (List<String>) accessMapping.get(PUBLISHERS_KEY);
-                List<String> administrators = (List<String>) accessMapping.get(ADMINS_KEY);
-                Map<String, Set<Integer>> collections = (Map<String, Set<Integer>>) accessMapping.get(COLLECTIONS_KEY);
-
-                AccessMapping updated = new AccessMapping();
-                updated.getDigitalPublishingTeam().addAll(digitalPublishingTeam);
-
-                dataVisualisationPublishers.stream()
-                        .forEach(dataVisUser -> {
-                            info().data("user", dataVisUser).log("Migrating user");
-                            updated.getDigitalPublishingTeam().add(dataVisUser);
-                        });
-
-                updated.getAdministrators().addAll(administrators);
-                updated.setCollections(collections);
-
-                try (OutputStream output = Files.newOutputStream(p)) {
-                    Serialiser.serialise(output, updated);
-                }
             }
         }
     }
--- zebedee-cms/src/test/java/com/github/onsdigital/zebedee/permissions/store/PermissionsStoreFileSystemImplTest.java
+++ zebedee-cms/src/test/java/com/github/onsdigital/zebedee/permissions/store/PermissionsStoreFileSystemImplTest.java
@@ -1,6 +1,5 @@
 package com.github.onsdigital.zebedee.permissions.store;
 
-import com.github.davidcarboni.restolino.json.Serialiser;
 import com.github.onsdigital.zebedee.permissions.model.AccessMapping;
 import org.junit.After;
 import org.junit.Before;
@@ -9,21 +8,10 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
-import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
-import static com.github.onsdigital.zebedee.permissions.store.PermissionsStoreFileSystemImpl.ADMINS_KEY;
-import static com.github.onsdigital.zebedee.permissions.store.PermissionsStoreFileSystemImpl.COLLECTIONS_KEY;
-import static com.github.onsdigital.zebedee.permissions.store.PermissionsStoreFileSystemImpl.DATA_VIS_KEY;
 import static com.github.onsdigital.zebedee.permissions.store.PermissionsStoreFileSystemImpl.PERMISSIONS_FILE;
-import static com.github.onsdigital.zebedee.permissions.store.PermissionsStoreFileSystemImpl.PUBLISHERS_KEY;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -57,46 +45,11 @@ public class PermissionsStoreFileSystemImplTest {
     }
 
     @Test
-    public void initialisePermissions_ShouldCreateAccessMappingIfDoesNotExist() throws Exception {
+    public void constructor_ShouldCreateAccessMappingIfDoesNotExist() throws Exception {
         PermissionsStoreFileSystemImpl.initialisePermissions(permissionsDir);
         PermissionsStore store = new PermissionsStoreFileSystemImpl(permissionsDir);
 
         assertThat(Files.exists(accessMappingPath.toPath()), is(true));
         assertThat(new AccessMapping(), equalTo(store.getAccessMapping()));
     }
-
-    @Test
-    public void initialisePermissions_ShouldMigrateDataVisUsers() throws Exception {
-        accessMappingPath.createNewFile();
-
-        Set<String> digitalPublishingTeam = new HashSet();
-        digitalPublishingTeam.add(publisher1);
-        digitalPublishingTeam.add(publisher2);
-        digitalPublishingTeam.add(publisher3);
-
-        Set<String> dataVisualisationPublishers = new HashSet();
-        dataVisualisationPublishers.add(dataVis1);
-        dataVisualisationPublishers.add(dataVis2);
-        dataVisualisationPublishers.add(dataVis3);
-
-        Map<String, Object> accessMapping = new HashMap();
-        accessMapping.put(PUBLISHERS_KEY, digitalPublishingTeam);
-        accessMapping.put(DATA_VIS_KEY, dataVisualisationPublishers);
-        accessMapping.put(ADMINS_KEY, new HashSet<String>());
-        accessMapping.put(COLLECTIONS_KEY, new HashMap<String, Set<Integer>>());
-
-        try (OutputStream output = Files.newOutputStream(accessMappingPath.toPath())) {
-            Serialiser.serialise(output, accessMapping);
-        }
-
-        AccessMapping expected = new AccessMapping();
-        expected.getDigitalPublishingTeam().addAll(dataVisualisationPublishers);
-        expected.getDigitalPublishingTeam().addAll(digitalPublishingTeam);
-
-        PermissionsStoreFileSystemImpl.initialisePermissions(permissionsDir);
-        PermissionsStore store = new PermissionsStoreFileSystemImpl(permissionsDir);
-
-        assertThat(expected, equalTo(store.getAccessMapping()));
-    }
-
 }

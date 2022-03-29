181aeb4da2008f8051b6c49d71d8c94562fd942d
AWitcherONS <ann.witcher@ons.gov.uk>
2022-03-29 10:55:19 +0100

    conflict resolution amendment

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/api/CollectionDetails.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/api/CollectionDetails.java
@@ -106,13 +106,12 @@ public class CollectionDetails {
         addEventsForDetails(result.complete, collection);
         addEventsForDetails(result.reviewed, collection);
 
-        Set<String> teamIds = zebedeeCmsService.getPermissions().listViewerTeams(session,
-                collection.getDescription().getId());
-        result.teamsDetails = zebedeeCmsService.getZebedee().getTeamsService()
-                                               .resolveTeamDetails(teamIds);
-        result.teamsDetails.forEach(team -> collection.getDescription().getTeams()
-                                                      .add(team.getName()));
+        Set<String> teamIds = zebedeeCmsService.getPermissions().listViewerTeams(session,collection.getDescription().getId());
 
+        if (!cmsFeatureFlags().isJwtSessionsEnabled()) {
+            result.teamsDetails = zebedeeCmsService.getZebedee().getTeamsService().resolveTeamDetails(teamIds);
+            result.teamsDetails.forEach(team -> collection.getDescription().getTeams().add(team.getName()));
+        }
         String collectionId = Collections.getCollectionId(request);
 
         if (datasetImportEnabled) {

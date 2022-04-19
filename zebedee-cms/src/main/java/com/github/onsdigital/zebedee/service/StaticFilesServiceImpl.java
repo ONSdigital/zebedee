4e586c52fd4faea409108d3f4ea4854709ef7df9
W. Brian Quinn <w.brian.quinn@gmail.com>
2022-04-19 18:21:15 +0100

    Implemented Static Files API client integration
    also using 1.1.1 of files API client, and using testing.

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/service/StaticFilesServiceImpl.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/service/StaticFilesServiceImpl.java
@@ -1,32 +1,30 @@
 package com.github.onsdigital.zebedee.service;
 
+import com.github.onsdigital.dp.files.api.Client;
 import com.github.onsdigital.zebedee.model.Collection;
+
 
 public class StaticFilesServiceImpl implements StaticFilesService {
 
 
+    private final Client client;
+
+    public StaticFilesServiceImpl(Client client) {
+        this.client = client;
+    }
+
     @Override
-    public void publishCollection(Collection collection) {
+    public void publishCollection(Collection collection){
+        if (collection.getDescription() == null) {
+            throw new IllegalArgumentException("Collection description was null.");
+        }
+
         String collectionId = collection.getDescription().getId();
 
-        if ( collectionId == null || collectionId.isEmpty() ) {
+        if (collectionId == null || collectionId.isEmpty()) {
             throw new IllegalArgumentException("A collectionId must be set in the collection being published.");
         }
 
-        try {
-            staticFilesAPI.publish(collectionId);
-        } catch (NoFilesFoundException noFilesFoundException)  {
-            info().log(noFilesFoundException, "");
-            throw nffe;
-        } catch (FilesInWrongStateException filesInWrongStateException) {
-            info().log(filesInWrongStateException, "");
-            throw filesInWrongStateException;
-
-        } catch (NotAuthorisedToPublishException notAuthorisedToPublishException) {
-            info().log(notAuthorisedToPublishException, "");
-            throw notAuthorisedToPublishException;
-        } catch (Exception e) {
-
-        }
+        client.publishCollection(collectionId);
     }
 }

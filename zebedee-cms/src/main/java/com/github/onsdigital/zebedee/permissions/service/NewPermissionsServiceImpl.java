2f66ee6e7a6e8f108ef012c96bd3e47a99441b51
Mariana Silva <Marianasilvago1234@gmail.com>
2022-07-25 10:47:19 +0530

    Import correct UserDataPayload

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/permissions/service/NewPermissionsServiceImpl.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/permissions/service/NewPermissionsServiceImpl.java
@@ -1,20 +1,34 @@
 package com.github.onsdigital.zebedee.permissions.service;
 
+import com.github.onsdigital.UserDataPayload;
+import com.github.onsdigital.dp.authorisation.permissions.PermissionChecker;
 import com.github.onsdigital.zebedee.exceptions.BadRequestException;
 import com.github.onsdigital.zebedee.exceptions.NotFoundException;
 import com.github.onsdigital.zebedee.exceptions.UnauthorizedException;
 import com.github.onsdigital.zebedee.exceptions.ZebedeeException;
 import com.github.onsdigital.zebedee.json.PermissionDefinition;
 import com.github.onsdigital.zebedee.session.model.Session;
+import org.joda.time.Duration;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Set;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import static java.text.MessageFormat.format;
 
 public class NewPermissionsServiceImpl implements PermissionsService {
-
+    private ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private final Lock readLock = readWriteLock.readLock();
+    private final Lock writeLock = readWriteLock.writeLock();
     private static final String UNSUPPORTED_ERROR = "New Permissions API is enabled: {0} is no longer supported";
+    private PermissionChecker permissionChecker;
+
+    public NewPermissionsServiceImpl(String permissionsAPIHost ) {
+        this.permissionChecker = new PermissionChecker(permissionsAPIHost, Duration.standardSeconds(10), Duration.standardSeconds(20), Duration.standardMinutes(30));
+    }
 
 
     @Override
@@ -46,8 +60,14 @@ public class NewPermissionsServiceImpl implements PermissionsService {
 
     @Override
     public boolean canEdit(Session session) throws IOException {
-        throw new UnsupportedOperationException(format(UNSUPPORTED_ERROR, "canEdit"));
-    }
+        boolean has_permission = false;
+        try {
+            has_permission = hasPermission(session, "legacy:edit", "");
+
+        } catch (Exception e){
+            return false;
+        }
+        return has_permission;    }
 
 
     @Override
@@ -63,8 +83,18 @@ public class NewPermissionsServiceImpl implements PermissionsService {
     }
 
     @Override
-    public boolean canView(Session session, String collectionId) throws IOException{
-        throw new UnsupportedOperationException(format(UNSUPPORTED_ERROR, "listViewerTeams"));
+    public boolean canView(Session session, String collectionId) throws IOException {
+        boolean has_permission = false;
+        readLock.lock();
+        try {
+            has_permission = hasPermission(session, "legacy:read", collectionId);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            readLock.unlock();
+        }
+        return has_permission;
     }
 
     @Override
@@ -85,7 +115,43 @@ public class NewPermissionsServiceImpl implements PermissionsService {
 
     @Override
     public PermissionDefinition userPermissions(Session session) throws IOException {
-        return null;
+        readLock.lock();
+        try {
+            boolean isEditor = canEdit(session);
+            boolean isAdmin = canUpdate(session);
+
+            return new PermissionDefinition()
+                    .isAdmin(isAdmin)
+                    .isEditor(isEditor);
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    private Boolean hasPermission(Session session, String permissionString, String collectionId) throws Exception {
+        HashMap<String, String> attributes = new HashMap<String, String>() {{
+            put("collection_id", collectionId);
+        }};
+
+
+        UserDataPayload userDataPayload =  new UserDataPayload(session.getId(),session.getEmail(), Arrays.asList(new String[]{"admin"}));
+
+        Boolean hasPermission = permissionChecker.hasPermission(userDataPayload, permissionString, attributes);
+
+        return hasPermission;
+    }
+    private boolean canUpdate(Session session) throws IOException {
+        boolean has_permission = false;
+        readLock.lock();
+        try {
+            has_permission = hasPermission(session, "users:update", "");
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            readLock.unlock();
+        }
+        return has_permission;
     }
 
 }

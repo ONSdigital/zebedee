6a72229c4a1e0311f66131b8a48dce37de0292ad
Rafa <rafael.garcia-pendon@ons.gov.uk>
2022-12-16 11:36:02 +0000

    Refactor to combine if statements

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/service/ContentStatusUtils.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/service/ContentStatusUtils.java
@@ -15,23 +15,26 @@ public class ContentStatusUtils {
     public static ContentStatus updatedStateInCollection(ContentStatus currentState, ContentStatus newState, String lastEditedBy, String user) throws ForbiddenException, BadRequestException {
 
         Objects.requireNonNull(newState);
+        
+        if (currentState == null) {
+            switch (newState) {
+            case Reviewed:
+                info().data("last edited by", lastEditedBy)
+                        .data("user", user)
+                        .log("Attempt to review a resource that hasn't been submitted for review");
 
-        if (currentState == null && newState.equals(ContentStatus.Reviewed)) {
-            info().data("last edited by", lastEditedBy).data("user", user)
-                    .log("Attempt to review a resource that hasn't been submitted for review");
+                throw new BadRequestException("Cannot be reviewed without being submitted for review first");
+            case InProgress:
+            case Complete:
+                // Updating from scratch to 'in progress' or 'complete' state so don't need to perform following checks
+                info().data("user", user)
+                        .data("last edited by", lastEditedBy)
+                        .data("current state", currentState)
+                        .data("new state", newState)
+                        .log("Updating resource state for first time");
 
-            throw new BadRequestException("Cannot be reviewed without being submitted for review first");
-        }
-
-        // Updating from scratch to 'in progress' or 'complete' state so don't need to perform following checks
-        if (currentState == null && (newState.equals(ContentStatus.InProgress) || newState.equals(ContentStatus.Complete))) {
-            info().data("user", user)
-                    .data("last edited by", lastEditedBy)
-                    .data("current state", currentState)
-                    .data("new state", newState)
-                    .log("Updating resource state for first time");
-
-            return newState;
+                return newState;
+            }
         }
 
         // Can't skip the Complete state
@@ -45,37 +48,35 @@ public class ContentStatusUtils {
             throw new ForbiddenException("Can't approve a resource in progress");
         }
 
-        // The same user can't review edits they've submitted for review
-        if (currentState.equals(ContentStatus.Complete) && newState.equals(ContentStatus.Reviewed) && lastEditedBy.equalsIgnoreCase(user)) {
-            info().data("user", user)
-                    .data("last edited by", lastEditedBy)
-                    .data("current state", currentState)
-                    .data("new state", newState)
-                    .log("User attempting to review their own resource");
+        if (currentState.equals(ContentStatus.Complete)) {
+            if (lastEditedBy.equalsIgnoreCase(user)) {
+                if (newState.equals(ContentStatus.Reviewed)) {
+                    // The same user can't review edits they've submitted for review
+                    info().data("user", user)
+                            .data("last edited by", lastEditedBy)
+                            .data("current state", currentState)
+                            .data("new state", newState)
+                            .log("User attempting to review their own resource");
 
-            throw new ForbiddenException("User " + user + "doesn't have permission to review a resource they completed");
-        }
-
-        // Any further updates made by the user who submitted the dataset should keep the dataset in the awaiting review state
-        if (currentState.equals(ContentStatus.Complete) && lastEditedBy.equalsIgnoreCase(user)) {
-
-            info().data("user", user)
-                    .data("last edited by", lastEditedBy)
-                    .data("current state", currentState)
-                    .data("new state", newState)
-                    .log("User making more updates to a resource whilst it is awaiting review");
-            return ContentStatus.Complete;
-        }
-
-        // Any updates to a dataset awaiting review by a different user means it moves back to an in progress state
-        if (currentState.equals(ContentStatus.Complete) && !newState.equals(ContentStatus.Reviewed) && !lastEditedBy.equalsIgnoreCase(user)) {
-
-            info().data("user", user)
-                    .data("last edited by", lastEditedBy)
-                    .data("current state", currentState)
-                    .data("new state", newState)
-                    .log("A different user making updates to a resource whilst it is awaiting review");
-            return ContentStatus.InProgress;
+                    throw new ForbiddenException("User " + user + "doesn't have permission to review a resource they completed");
+                } else {
+                    // Any further updates made by the user who submitted the dataset should keep the dataset in the awaiting review state
+                    info().data("user", user)
+                            .data("last edited by", lastEditedBy)
+                            .data("current state", currentState)
+                            .data("new state", newState)
+                            .log("User making more updates to a resource whilst it is awaiting review");
+                    return ContentStatus.Complete;
+                }
+            } else if (newState.equals(ContentStatus.InProgress) || newState.equals(ContentStatus.Complete)) {
+                // Any updates to a dataset awaiting review by a different user means it moves back to an in progress state
+                info().data("user", user)
+                        .data("last edited by", lastEditedBy)
+                        .data("current state", currentState)
+                        .data("new state", newState)
+                        .log("A different user making updates to a resource whilst it is awaiting review");
+                return ContentStatus.InProgress;
+            }
         }
 
         info().data("user", user)

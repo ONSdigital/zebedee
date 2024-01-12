81920a9250e5567bfddd7496d77d7a8e6dc50dca
Dionisi Lola <dionisislolas@gmail.com>
2024-01-12 08:37:08 +0000

    Remove references to interactives from collection details

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/api/CollectionDetails.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/api/CollectionDetails.java
@@ -125,8 +125,6 @@ public class CollectionDetails {
             result.datasetVersions = collection.getDescription().getDatasetVersions();
         }
 
-        result.interactives = collection.getDescription().getInteractives();
-
         return result;
     }
 

c5def3ce3f1edaca7ccd82ae8a8295d65898db98
janderson2 <janderson2@users.noreply.github.com>
2026-02-02 18:06:51 +0000

    Replace deprecated isPublisher with canEdit
    Replace usages of deprecated PermissionsService.isPublisher method
    with the PermissionsService.canEdit method in preparation of further
    work on the PermissionsService.

--- zebedee-cms/src/main/java/com/github/onsdigital/zebedee/model/Collections.java
+++ zebedee-cms/src/main/java/com/github/onsdigital/zebedee/model/Collections.java
@@ -1017,7 +1017,7 @@ public class Collections {
             return false;
         }
 
-        if (!permissionsService.isPublisher(session)) {
+        if (!permissionsService.canEdit(session)) {
             info().user(session)
                     .reason("publisher permissions required")
                     .log("dataset version validation not bypassed");
